370dfcc5036ef5cdd63ec2f038f1f448af3426ff
charles <charles@localhost>
2004-04-26 15:24:47 +0000

    *** empty log message ***

--- source/org/martus/client/test/TestClient.java
+++ source/org/martus/client/test/TestClient.java
@@ -62,6 +62,7 @@ public class TestClient
 		suite.addTest(new TestSuite(TestDateUtilities.class));
 		suite.addTest(new TestSuite(TestFolderList.class));
 		suite.addTest(new TestSuite(TestGridTableModel.class));
+		suite.addTest(new TestSuite(testHQKeys.class));
 		suite.addTest(new TestSuite(TestMartusApp_NoServer.class));
 		suite.addTest(new TestSuite(TestMartusFlexidate.class));
 		suite.addTest(new TestSuite(TestLocalization.class));
