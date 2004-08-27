058d199a19b196a0d6682e0c2a782eae90948bbe
ivo <ivo@localhost>
2004-08-27 00:27:28 +0000

    corrected version for final build

--- source/org/martus/client/swingui/UiConstants.java
+++ source/org/martus/client/swingui/UiConstants.java
@@ -35,7 +35,7 @@ public class UiConstants
 	// Also, you should search for "Martus" in EnglishStrings.txt and any .mtf files 
 	// and change other occurances as required by the license.
 	public static final String programName = "Martus(TM)";
-	public static final String versionLabel = "internal pre-release 2.0.1";
+	public static final String versionLabel = "2.0.1";
 	public static final String copyright = "Copyright 2001-2004 Beneficent Technology, Inc.";
 	public static final String website = "(Benetech, at www.benetech.org)";
 }
