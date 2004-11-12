6d53d5697dd3dd1cd951790fc1143bc7137281bc
charles <charles@localhost>
2004-11-12 01:13:39 +0000

    changed to internal and made it non final so only 1 file gets changed.

--- source/org/martus/client/swingui/UiConstants.java
+++ source/org/martus/client/swingui/UiConstants.java
@@ -35,7 +35,7 @@ public class UiConstants
 	// Also, you should search for "Martus" in EnglishStrings.txt and any .mtf files 
 	// and change other occurances as required by the license.
 	public static final String programName = "Martus(TM)";
-	public static final String versionLabel = "2.5";
+	public static String versionLabel = "Internal 3.0";
 	public static final String copyright = "Copyright 2001-2004 Beneficent Technology, Inc.";
 	public static final String website = "(Benetech, at www.benetech.org)";
 }
