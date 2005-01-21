fde6da4535656c7489915898ce0114bd362d6981
kevins <kevins@localhost>
2005-01-21 02:46:53 +0000

    Work around Arabic descenders getting chopped off

--- source/org/martus/client/swingui/fields/UiNormalTextEditor.java
+++ source/org/martus/client/swingui/fields/UiNormalTextEditor.java
@@ -26,10 +26,13 @@ Boston, MA 02111-1307, USA.
 
 package org.martus.client.swingui.fields;
 
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
 import javax.swing.JComponent;
+
 import org.martus.client.swingui.UiConstants;
 import org.martus.client.swingui.UiLocalization;
 import org.martus.swing.UiTextArea;
@@ -39,17 +42,42 @@ public class UiNormalTextEditor extends UiNormalTextField
 	public UiNormalTextEditor(UiLocalization localizationToUse)
 	{
 		super(localizationToUse);
-		widget = new UiTextArea(1, UiConstants.textFieldColumns);
+		widget = new UiTextAreaWithPadding(1, UiConstants.textFieldColumns);
 		widget.setLineWrap(true);
 		widget.setWrapStyleWord(true);
 		widget.setFont(new Font("SansSerif", Font.PLAIN, UiConstants.defaultFontSize));
 		widget.addKeyListener(new myKeyListener());
 		supportContextMenu();
 	}
+	
+	class UiTextAreaWithPadding extends UiTextArea
+	{
+		public UiTextAreaWithPadding(int rows, int cols)
+		{
+			super(rows, cols);
+		}
+
+		/* 
+		 * NOTE: This is a horrible hack to work around the fact that a JTextArea
+		 * seems to completely ignore any calls to setBorder or setMargin.
+		 * We need to add some space to the bottom of the field to avoid Arabic
+		 * (and even English) characters from being chopped off. 
+		 * The number of pixels is arbitrary and may need to be adjusted.
+		 */
+		public Dimension getPreferredSize()
+		{
+			final int EXTRA_PIXELS = 3;
+			Dimension d = super.getPreferredSize();
+			d.setSize(d.getWidth(), d.getHeight() + EXTRA_PIXELS);
+			return d;
+		}
+	}
+	
 	public JComponent[] getFocusableComponents()
 	{
 		return new JComponent[]{widget};
 	}
+	
 	class myKeyListener implements KeyListener
 	{
 		public void keyPressed(KeyEvent e)
