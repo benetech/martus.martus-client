49c8d4f802cfbc686544cc675fc1b8b5be28ec1c
kevins <kevins@localhost>
2005-06-16 19:08:32 +0000

    Add strings for fancy search dialog

--- source/org/martus/client/swingui/EnglishStrings.java
+++ source/org/martus/client/swingui/EnglishStrings.java
@@ -451,6 +451,11 @@ public class EnglishStrings
 "field:HeadQuartersSelected=Selected?",
 "field:ConfigureHeadQuartersDefault=Default?",
 "field:CustomizationTemplateFiles=Customization Template Files",
+"field:SearchGridHeaderField=Field(s) to search",
+"field:SearchGridHeaderOp=Compare how?",
+"field:SearchGridHeaderValue=Search for...",
+"field:SearchOpContains=contains",
+"field:SearchAnyField=Any Field",
 
 "field:PrintPrivateData=Include private data in print out.",
 "field:PrintPrivateDataMessage=You have the option to include or exclude the selected bulletin's private information in this print out.\n\nTo only print public information, do not mark the checkbox below.\n\nIf you do mark the checkbox, anyone who sees the print out will be able to read your private data.",
