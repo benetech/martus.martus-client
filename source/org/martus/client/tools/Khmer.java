f573ecb69caa36b9bcd02c2e5fc62741b6b2b8c0
kevins <kevins@localhost>
2005-06-23 03:22:40 +0000

    Add khmer display using TextLayout

--- source/org/martus/client/tools/Khmer.java
+++ source/org/martus/client/tools/Khmer.java
@@ -26,9 +26,16 @@ Boston, MA 02111-1307, USA.
 
 package org.martus.client.tools;
 
-import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
+import javax.swing.Box;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 
@@ -36,17 +43,52 @@ public class Khmer
 {
 	public static void main(String[] args)
 	{
-		char[] khmerText = {0x1781, 0x17D2, 0x1789, 0x17BB, 0x17C6, 0x200B, 0x179F, 0x17D2, 0x179A, 0x17B6, 0x200B, 0x179C, 0x17B7, 0x1791, 0x17BC};
-		JLabel khmerLabel = new JLabel(new String(khmerText));
+		char[] khmerText = {0x1781, 0x17D2, 0x1789, 0x17BB, 0x17C6, 0x200B, 
+							0x179F, 0x17D2, 0x179A, 0x17B6, 0x200B, 
+							0x179C, 0x17B7, 0x1791, 0x17BC};
+		String khmerString = "Khmer: " + new String(khmerText);
 		Font khmerFont = new Font("Khmer OS", Font.PLAIN, 30);
+
+		JLabel khmerLabel = new JLabel(khmerString);
 		khmerLabel.setFont(khmerFont);
 
+		Box vbox = Box.createVerticalBox();
+		vbox.add(khmerLabel);
+		vbox.add(new KhmerViaTextLayout(khmerString, khmerFont));
+
 		JDialog sample = new JDialog();
-		sample.setTitle(new String(khmerText));
-		sample.getContentPane().add(khmerLabel, BorderLayout.PAGE_START);
-		sample.setSize(350,150);
+		sample.setTitle(khmerString);
+		sample.getContentPane().add(vbox);
+		sample.setSize(600,300);
 		sample.setModal(true);
 		sample.setVisible(true);
 	}
+}
 
+class KhmerViaTextLayout extends JComponent
+{
+	public KhmerViaTextLayout(String textToShow, Font f)
+	{
+		text = textToShow;
+		setFont(f);
+	}
+	
+	public void paint(Graphics graphics)
+	{
+		Graphics2D g = (Graphics2D)graphics;
+		Point2D loc = new Point2D.Float(0, 50);
+		Font font = getFont();
+		FontRenderContext frc = g.getFontRenderContext();
+		TextLayout layout = new TextLayout(text, font, frc);
+		layout.draw(g, (float)loc.getX(), (float)loc.getY());
+		
+		Rectangle2D bounds = layout.getBounds();
+		bounds.setRect(bounds.getX()+loc.getX(),
+		bounds.getY()+loc.getY(),
+		bounds.getWidth(),
+		bounds.getHeight());
+		g.draw(bounds);
+	}
+	
+	String text;
 }
