8f814eb741555eddbe5365e4cdf96f4126a91490
charles <charles@localhost>
2006-03-22 22:30:36 +0000

    Changed Client version # to 2.9 <Anna, with help from Kevin>

--- source/org/martus/client/swingui/UiConstants.java
+++ source/org/martus/client/swingui/UiConstants.java
@@ -32,7 +32,7 @@ public class UiConstants
 	// Also, you should search for "Martus" in EnglishStrings.txt and any .mtf files 
 	// and change other occurances as required by the license.
 	public static final String programName = "Martus(TM)";
-	public static String versionLabel = "pre-2.9 internal";
+	public static String versionLabel = "2.9";
 	public static final String copyright = "Copyright 2001-2006 Beneficent Technology, Inc.";
 	public static final String website = "(Benetech, at www.benetech.org)";
 
