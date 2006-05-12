d6984c5ee532c49e9dc7333f05a4efa119a9955e
charles <charles@localhost>
2006-05-12 15:17:45 +0000

    move date simple/range tags out of the exporter and into the actual date classes so it can be inherited within grids.

--- source/org/martus/client/core/BulletinXmlExporter.java
+++ source/org/martus/client/core/BulletinXmlExporter.java
@@ -182,16 +182,6 @@ public class BulletinXmlExporter
 				String valueTagAndData = MartusXml.getTagWithData(BulletinXmlConstants.VALUE, value);
 				writeElementDirect(dest, tag, valueTagAndData);
 			}
-			if(spec.getType().isDate())
-			{
-				String simpleDateValue = BulletinXmlConstants.DATE_SIMPLE+ value;
-				writeElement(dest, tag, simpleDateValue);				
-			}
-			if(spec.getType().isDateRange())
-			{
-				String simpleDateValue = BulletinXmlConstants.DATE_RANGE+ value;
-				writeElement(dest, tag, simpleDateValue);				
-			}
 			else
 			{
 				writeElement(dest, tag, value);
