08f09f838938a90cec9b21576ba18d79b42195e3
kevins <kevins@localhost>
2006-08-14 18:15:40 +0000

    Add Footer and FakePageBreak sections (not used yet)

--- source/org/martus/client/reports/ReportFormat.java
+++ source/org/martus/client/reports/ReportFormat.java
@@ -45,17 +45,29 @@ public class ReportFormat
 	public ReportFormat(JSONObject json)
 	{
 		version = json.optInt(TAG_VERSION, 0);
+		setBulletinPerPage(json.optBoolean(TAG_BULLETIN_PER_PAGE));
 		setStartSection(json.getString(TAG_START_SECTION));
 		setHeaderSection(json.optString(TAG_HEADER_SECTION, ""));
 		setDetailSection(json.getString(TAG_DETAIL_SECTION));
 		setBreakSection(json.optString(TAG_BREAK_SECTION, ""));
+		setFooterSection(json.optString(TAG_FOOTER_SECTION, ""));
+		setFakePageBreakSection(json.optString(TAG_FAKE_PAGE_BREAK_SECTION, ""));
 		setEndSection(json.getString(TAG_END_SECTION));
-		setBulletinPerPage(json.optBoolean(TAG_BULLETIN_PER_PAGE));
 	}
 	
 	public int getVersion()
 	{
 		return version;
+	}
+	
+	public void setBulletinPerPage(boolean newSetting)
+	{
+		bulletinPerPage = newSetting;
+	}
+	
+	public boolean getBulletinPerPage()
+	{
+		return bulletinPerPage;
 	}
 	
 	public void setStartSection(String section)
@@ -68,14 +80,14 @@ public class ReportFormat
 		return startSection;
 	}
 	
-	public void setEndSection(String section)
+	public void setHeaderSection(String section)
 	{
-		endSection = section;
+		headerSection = section;
 	}
 	
-	public String getEndSection()
+	public String getHeaderSection()
 	{
-		return endSection;
+		return headerSection;
 	}
 	
 	public void setDetailSection(String section)
@@ -98,54 +110,108 @@ public class ReportFormat
 		return breakSection;
 	}
 	
-	public void setHeaderSection(String section)
+	public void setFooterSection(String section)
 	{
-		headerSection = section;
+		footerSection = section;
 	}
 	
-	public String getHeaderSection()
+	public String getFooterSection()
 	{
-		return headerSection;
+		return footerSection;
 	}
 	
-	public void setBulletinPerPage(boolean newSetting)
+	public void setFakePageBreakSection(String section)
 	{
-		bulletinPerPage = newSetting;
+		fakePageBreakSection = section;
 	}
 	
-	public boolean getBulletinPerPage()
+	public String getFakePageBreakSection()
 	{
-		return bulletinPerPage;
+		return fakePageBreakSection;
+	}
+	
+	public void setEndSection(String section)
+	{
+		endSection = section;
+	}
+	
+	public String getEndSection()
+	{
+		return endSection;
 	}
 	
 	public JSONObject toJson()
 	{
 		JSONObject json = new JSONObject();
 		json.put(TAG_VERSION, EXPECTED_VERSION);
-		json.put(TAG_START_SECTION, getStartSection());
-		json.put(TAG_DETAIL_SECTION, getDetailSection());
-		json.put(TAG_END_SECTION, getEndSection());
-		json.put(TAG_BREAK_SECTION, getBreakSection());
-		json.put(TAG_HEADER_SECTION, getHeaderSection());
 		json.put(TAG_BULLETIN_PER_PAGE, getBulletinPerPage());
+		json.put(TAG_START_SECTION, getStartSection());
+		json.put(TAG_HEADER_SECTION, getHeaderSection());
+		json.put(TAG_DETAIL_SECTION, getDetailSection());
+		json.put(TAG_BREAK_SECTION, getBreakSection());
+		json.put(TAG_FOOTER_SECTION, getFooterSection());
+		json.put(TAG_FAKE_PAGE_BREAK_SECTION, getFakePageBreakSection());
+		json.put(TAG_END_SECTION, getEndSection());
 		return json;
 	}
 	
 	final static String TAG_VERSION = "Version";
-	final static String TAG_START_SECTION = "StartSection";
-	final static String TAG_DETAIL_SECTION = "DetailSection";
-	final static String TAG_END_SECTION = "EndSection";
-	final static String TAG_BREAK_SECTION = "BreakSection";
-	final static String TAG_HEADER_SECTION = "HeaderSection";
 	final static String TAG_BULLETIN_PER_PAGE = "BulletinPerPage";
+	final static String TAG_START_SECTION = "StartSection";
+	final static String TAG_HEADER_SECTION = "HeaderSection";
+	final static String TAG_DETAIL_SECTION = "DetailSection";
+	final static String TAG_BREAK_SECTION = "BreakSection";
+	final static String TAG_FOOTER_SECTION = "FooterSection";
+	final static String TAG_FAKE_PAGE_BREAK_SECTION = "FakePageBreak";
+	final static String TAG_END_SECTION = "EndSection";
 	
-	public final static int EXPECTED_VERSION = 5;
+	public final static int EXPECTED_VERSION = 6;
 
 	private int version;
 	private String startSection;
 	private String headerSection;
 	private String detailSection;
 	private String breakSection;
+	private String footerSection;
+	private String fakePageBreakSection;
 	private String endSection;
+	
+	/* 
+	 * tabular to disk or printer:
+	 *  - start <html><table>
+	 *  
+	 *  - header <tr><th></th></tr>
+	 *  - detail, detail, detail, break,
+	 *  - footer (empty)
+	 *  
+	 *  - end </table></html>
+	 *  
+	 * page to disk (multiple bulletins):
+	 *  - start <html>
+	 *  
+	 *  - header <table>
+	 *  - detail, break,
+	 *  - footer </table>
+	 *  
+	 *  - fakePageBreak <hr/>
+	 *  
+	 *  - header <table>
+	 *  - detail, break,
+	 *  - footer </table>
+	 *  
+	 *  - end </html>
+	 * 
+	 * page to printer (for each bulletin):
+	 *  - start <html>
+	 *  
+	 *  - header <table>
+	 *  - detail, break,
+	 *  - footer </table>
+	 *  
+	 *  - end </html>
+	 * 
+	 */
+		
+	
 	private boolean bulletinPerPage;
 }
