72633d6de34c91f270dcd25df44084169067cbb7
kevins <kevins@localhost>
2006-08-14 19:04:29 +0000

    Refactor report runner to make logic more clear

--- source/org/martus/client/reports/ReportRunner.java
+++ source/org/martus/client/reports/ReportRunner.java
@@ -39,7 +39,9 @@ import org.martus.client.core.SortableBulletinList;
 import org.martus.common.MiniLocalization;
 import org.martus.common.bulletin.Bulletin;
 import org.martus.common.bulletin.BulletinLoader;
+import org.martus.common.bulletin.Bulletin.DamagedBulletinException;
 import org.martus.common.crypto.MartusCrypto;
+import org.martus.common.crypto.MartusCrypto.NoKeyPairException;
 import org.martus.common.database.DatabaseKey;
 import org.martus.common.database.ReadableDatabase;
 import org.martus.common.field.MartusField;
@@ -61,76 +63,57 @@ public class ReportRunner
 	
 	public void runReport(ReportFormat rf, ReadableDatabase db, SortableBulletinList bulletins, ReportOutput destination, RunReportOptions options) throws Exception
 	{
-		ReportOutput nullReportOutput = new ReportOutput();
-		
 		UniversalId[] uids = bulletins.getSortedUniversalIds();
+		SummaryBreakHandler breakHandler = new SummaryBreakHandler(rf, destination, options, bulletins.getSortSpecs());
 
-		Context context = new VelocityContext();
+		VelocityContext context = new VelocityContext();
 		context.put("localization", localization);
-		
-		MiniFieldSpec[] breakSpecs = bulletins.getSortSpecs();
-		if(!options.printBreaks)
-			breakSpecs = new MiniFieldSpec[0];
-		
-		String[] previousBreakValues = new String[breakSpecs.length];
-		Arrays.fill(previousBreakValues, "");
-		int[] breakCounts = new int[breakSpecs.length];
-		Arrays.fill(breakCounts, 0);
 		
 		performMerge(rf.getStartSection(), destination, context);
 
 		ReportOutput headerDestination = destination;
 		if(options.hideDetail)
-			headerDestination = nullReportOutput;
+			headerDestination = new ReportOutput();
 		performMerge(rf.getHeaderSection(), headerDestination, context);
 
 		for(int bulletin = 0; bulletin < uids.length; ++bulletin)
 		{
-			DatabaseKey key = DatabaseKey.createLegacyKey(uids[bulletin]);
-			Bulletin b = BulletinLoader.loadFromDatabase(db, key, signatureVerifier);
-			SafeReadableBulletin safeReadableBulletin = new SafeReadableBulletin(b, localization);
-			if(!options.includePrivate)
-				safeReadableBulletin.removePrivateData();
+			SafeReadableBulletin safeReadableBulletin = getCensoredBulletin(db, uids[bulletin], options);
 			
-			for(int breakLevel = breakSpecs.length - 1; breakLevel >= 0; --breakLevel)
-			{
-				String current = "";
-				MartusField thisField = safeReadableBulletin.getPossiblyNestedField(breakSpecs[breakLevel]);
-				if(thisField != null)
-					current = thisField.getData();
-
-				if(!current.equals(previousBreakValues[breakLevel]))
-				{
-					if(bulletin > 0)
-						performBreak(rf, context, destination, breakSpecs, previousBreakValues, breakLevel, breakCounts[breakLevel]);
-					previousBreakValues[breakLevel] = current;
-					breakCounts[breakLevel] = 0;
-				}
-			}
-			
-			context.put("i", new Integer(bulletin+1));
-			context.put("bulletin", safeReadableBulletin);
-			
-			ReportOutput detailDestination = destination;
-			if(options.hideDetail)
-				detailDestination = nullReportOutput;
-			
-			if(rf.getBulletinPerPage() && bulletin > 0)
-				destination.startNewPage();
-			
-			performMerge(rf.getDetailSection(), detailDestination, context);
-			
-			for(int breakLevel = breakSpecs.length - 1; breakLevel >= 0; --breakLevel)
-			{
-				++breakCounts[breakLevel];
-			}
-			
-			context.remove("bulletin");
+			breakHandler.doBreak(context, safeReadableBulletin);
+			doDetail(rf, destination, options, context, bulletin, safeReadableBulletin);
+			breakHandler.incrementCounts();
 		}
-		for(int breakLevel = breakSpecs.length - 1; breakLevel >= 0; --breakLevel)
-			performBreak(rf, context, destination, breakSpecs, previousBreakValues, breakLevel, breakCounts[breakLevel]);
+		
+		breakHandler.doFinalBreak(context);
 		
 		performMerge(rf.getEndSection(), destination, context);
+	}
+
+	private void doDetail(ReportFormat rf, ReportOutput destination, RunReportOptions options, VelocityContext context, int bulletin, SafeReadableBulletin safeReadableBulletin) throws Exception
+	{
+		context.put("i", new Integer(bulletin+1));
+		context.put("bulletin", safeReadableBulletin);
+		
+		ReportOutput detailDestination = destination;
+		if(options.hideDetail)
+			detailDestination = new ReportOutput();
+		
+		if(rf.getBulletinPerPage() && bulletin > 0)
+			destination.startNewPage();
+		
+		performMerge(rf.getDetailSection(), detailDestination, context);
+		context.remove("bulletin");
+	}
+
+	private SafeReadableBulletin getCensoredBulletin(ReadableDatabase db, UniversalId uid, RunReportOptions options) throws IOException, DamagedBulletinException, NoKeyPairException
+	{
+		DatabaseKey key = DatabaseKey.createLegacyKey(uid);
+		Bulletin b = BulletinLoader.loadFromDatabase(db, key, signatureVerifier);
+		SafeReadableBulletin safeReadableBulletin = new SafeReadableBulletin(b, localization);
+		if(!options.includePrivate)
+			safeReadableBulletin.removePrivateData();
+		return safeReadableBulletin;
 	}
 
 	static class NullReportOutput extends ReportOutput
@@ -149,23 +132,89 @@ public class ReportRunner
 	
 	}
 	
-	private void performBreak(ReportFormat rf, Context context, Writer destination, MiniFieldSpec[] breakSpecs, String[] previousBreakValues, int breakLevel, int breakCount) throws Exception
+	class SummaryBreakHandler
 	{
-		BreakFields breakFields = new BreakFields();
-		for(int i = 0; i < breakLevel + 1; ++i)
+		public SummaryBreakHandler(ReportFormat rf, ReportOutput destination, RunReportOptions options, MiniFieldSpec[] breakSpecsToUse)
 		{
-			MiniFieldSpec miniSpec = breakSpecs[i];
-			FieldSpec spec = miniSpec.getType().createEmptyFieldSpec();
-			spec.setTag(miniSpec.getTag());
-			spec.setLabel(miniSpec.getLabel());
-			MartusField field = new MartusField(spec);
-			field.setData(previousBreakValues[i]);
-			breakFields.add(field);
+			output = destination;
+			
+			breakSection = rf.getBreakSection();
+			breakSpecs = breakSpecsToUse;
+			if(!options.printBreaks)
+				breakSpecs = new MiniFieldSpec[0];
+			
+			previousBreakValues = new String[breakSpecs.length];
+			Arrays.fill(previousBreakValues, "");
+			breakCounts = new int[breakSpecs.length];
+			Arrays.fill(breakCounts, 0);
+			
+
 		}
-		context.put("BreakCount", new Integer(breakCount));
-		context.put("BreakLevel", new Integer(breakLevel));
-		context.put("BreakFields", breakFields);
-		performMerge(rf.getBreakSection(), destination, context);
+		
+		public void doBreak(VelocityContext context, SafeReadableBulletin upcomingBulletin) throws Exception
+		{
+			for(int breakLevel = breakSpecs.length - 1; breakLevel >= 0; --breakLevel)
+			{
+				String current = getBreakData(upcomingBulletin, breakLevel);
+				if(current == null || !current.equals(previousBreakValues[breakLevel]))
+				{
+					if(breakCounts[0] > 0)
+						performBreak(context, breakLevel);
+					previousBreakValues[breakLevel] = current;
+					breakCounts[breakLevel] = 0;
+				}
+			}
+		}
+		
+		public void doFinalBreak(VelocityContext context) throws Exception
+		{
+			doBreak(context, null);
+		}
+
+		private String getBreakData(SafeReadableBulletin upcomingBulletin, int breakLevel)
+		{
+			if(upcomingBulletin == null)
+				return null;
+			
+			String current = "";
+			MartusField thisField = upcomingBulletin.getPossiblyNestedField(breakSpecs[breakLevel]);
+			if(thisField != null)
+				current = thisField.getData();
+			return current;
+		}
+		
+		private void performBreak(VelocityContext context, int breakLevel) throws Exception
+		{
+			BreakFields breakFields = new BreakFields();
+			for(int i = 0; i < breakLevel + 1; ++i)
+			{
+				MiniFieldSpec miniSpec = breakSpecs[i];
+				FieldSpec spec = miniSpec.getType().createEmptyFieldSpec();
+				spec.setTag(miniSpec.getTag());
+				spec.setLabel(miniSpec.getLabel());
+				MartusField field = new MartusField(spec);
+				field.setData(previousBreakValues[i]);
+				breakFields.add(field);
+			}
+			context.put("BreakLevel", new Integer(breakLevel));
+			context.put("BreakCount", new Integer(breakCounts[breakLevel]));
+			context.put("BreakFields", breakFields);
+			performMerge(breakSection, output, context);
+		}
+		
+		public void incrementCounts()
+		{
+			for(int breakLevel = breakSpecs.length - 1; breakLevel >= 0; --breakLevel)
+			{
+				++breakCounts[breakLevel];
+			}
+		}
+		
+		ReportOutput output;
+		String breakSection;
+		MiniFieldSpec[] breakSpecs;
+		int[] breakCounts;
+		String[] previousBreakValues;
 	}
 	
 	public void performMerge(String template, Writer result, Context context) throws Exception
