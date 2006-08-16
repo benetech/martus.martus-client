75971e4c7b74605d66666ccf3c1415fc4b562f5f
charles <charles@localhost>
2006-08-16 19:11:25 +0000

    Moved out two dialogs from the actionReport into their own dialog's package

--- source/org/martus/client/swingui/actions/ActionMenuReports.java
+++ source/org/martus/client/swingui/actions/ActionMenuReports.java
@@ -25,16 +25,9 @@ Boston, MA 02111-1307, USA.
 */
 package org.martus.client.swingui.actions;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Container;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Vector;
 
-import javax.swing.Box;
-import javax.swing.JDialog;
 import javax.swing.filechooser.FileFilter;
 
 import org.json.JSONObject;
@@ -48,31 +41,24 @@ import org.martus.client.reports.ReportRunner;
 import org.martus.client.reports.RunReportOptions;
 import org.martus.client.reports.TabularReportBuilder;
 import org.martus.client.search.SearchTreeNode;
-import org.martus.client.search.SortFieldChooserSpecBuilder;
 import org.martus.client.swingui.MartusLocalization;
 import org.martus.client.swingui.UiMainWindow;
 import org.martus.client.swingui.WorkerThread;
 import org.martus.client.swingui.dialogs.UiIncludePrivateDataDlg;
 import org.martus.client.swingui.dialogs.UiPrintPreviewDlg;
 import org.martus.client.swingui.dialogs.UiReportFieldOrganizerDlg;
-import org.martus.client.swingui.fields.UiPopUpTreeEditor;
+import org.martus.client.swingui.dialogs.UiRunOrCreateReportDlg;
+import org.martus.client.swingui.dialogs.UiSortFieldsDlg;
 import org.martus.clientside.FileDialogHelpers;
 import org.martus.clientside.FormatFilter;
-import org.martus.clientside.UiLocalization;
 import org.martus.common.MiniLocalization;
 import org.martus.common.bulletin.Bulletin;
-import org.martus.common.fieldspec.ChoiceItem;
 import org.martus.common.fieldspec.FieldSpec;
 import org.martus.common.fieldspec.FieldTypeBoolean;
 import org.martus.common.fieldspec.MiniFieldSpec;
-import org.martus.common.fieldspec.PopUpTreeFieldSpec;
 import org.martus.common.fieldspec.StandardFieldSpecs;
 import org.martus.swing.PrintUtilities;
-import org.martus.swing.UiButton;
-import org.martus.swing.UiComboBox;
 import org.martus.swing.UiLabel;
-import org.martus.swing.UiWrappedTextArea;
-import org.martus.swing.Utilities;
 import org.martus.util.UnicodeReader;
 import org.martus.util.UnicodeWriter;
 
@@ -100,7 +86,7 @@ public class ActionMenuReports extends ActionPrint
 			String createPageReportButtonLabel = localization.getButtonLabel("CreatePageReport");
 			String cancelButtonLabel = localization.getButtonLabel("cancel");
 			String[] buttonLabels = {runButtonLabel, createTabularReportButtonLabel, createPageReportButtonLabel, cancelButtonLabel, };
-			RunOrCreateReportDialog runOrCreate = new RunOrCreateReportDialog(mainWindow, buttonLabels);
+			UiRunOrCreateReportDlg runOrCreate = new UiRunOrCreateReportDlg(mainWindow, buttonLabels);
 			runOrCreate.setVisible(true);
 			String pressed = runOrCreate.getPressedButtonLabel();
 			if(pressed == null || pressed.equals(cancelButtonLabel))
@@ -221,7 +207,7 @@ public class ActionMenuReports extends ActionPrint
 		if(searchTree == null)
 			return;
 		
-		SortFieldsDialog sortDlg = new SortFieldsDialog(mainWindow);
+		UiSortFieldsDlg sortDlg = new UiSortFieldsDlg(mainWindow);
 		sortDlg.setVisible(true);
 		if(!sortDlg.ok())
 			return;
@@ -371,188 +357,7 @@ public class ActionMenuReports extends ActionPrint
 			return selectedSpecs;
 		}
 	}
-
 	
 	
-	static class RunOrCreateReportDialog extends JDialog implements ActionListener
-	{
-		public RunOrCreateReportDialog(UiMainWindow mainWindow, String[] buttonLabels)
-		{
-			super(mainWindow);
-			setModal(true);
-			setTitle(mainWindow.getLocalization().getWindowTitle("RunOrCreateReport"));
-			Container panel = getContentPane();
-			Box buttonBox = Box.createVerticalBox();
-			for(int i = 0; i < buttonLabels.length; ++i)
-			{
-				UiButton button = new UiButton(buttonLabels[i]);
-				button.addActionListener(this);
-				buttonBox.add(button);
-			}
-			
-			panel.add(buttonBox);
-			pack();
-			Utilities.centerDlg(this);
-		}
-		
-		public String getPressedButtonLabel()
-		{
-			return pressedButtonLabel;
-		}
-
-		public void actionPerformed(ActionEvent event)
-		{
-			UiButton button = (UiButton)event.getSource();
-			pressedButtonLabel = button.getText();
-			dispose();
-		}
-		
-		String pressedButtonLabel;
-	}
-	
-	static class SortFieldsDialog extends JDialog implements ActionListener
-	{
-		public SortFieldsDialog(UiMainWindow mainWindow)
-		{
-			super(mainWindow);
-			
-			if(sortMiniSpecs == null)
-				sortMiniSpecs = new Vector();
-			
-			setModal(true);
-			Container contentPane = getContentPane();
-			contentPane.setLayout(new BorderLayout());
-			UiLocalization localization = mainWindow.getLocalization();
-			setTitle(localization.getWindowTitle("ReportChooseSortFields"));
-
-			String text = localization.getFieldLabel("ReportChooseSortFields");
-
-			SortFieldChooserSpecBuilder builder = new SortFieldChooserSpecBuilder(localization);
-			PopUpTreeFieldSpec spec = builder.createSpec(mainWindow.getStore());
-			
-			contentPane.add(new UiWrappedTextArea(text), BorderLayout.BEFORE_FIRST_LINE);
-
-			Box multiSortBox = Box.createVerticalBox();
-			
-			sortChooser = new UiPopUpTreeEditor[MAX_SORT_LEVELS];
-			for(int i = 0; i < sortChooser.length; ++i)
-			{
-				sortChooser[i] = createSortChooser(mainWindow, spec);
-				multiSortBox.add(sortChooser[i].getComponent());
-			}
-			
-			for(int i = 0; i < sortChooser.length; ++i)
-			{
-				MiniFieldSpec selectedSpec = null;
-				if(i < sortMiniSpecs.size())
-					selectedSpec = (MiniFieldSpec)sortMiniSpecs.get(i);
-				sortChooser[i].select(selectedSpec);
-			}
-			
-			detailOnlyChoice = createChoiceItem("ReportDetailOnly", localization);
-			detailAndBreaksChoice = createChoiceItem("ReportDetailWithSummaries", localization);
-			breaksOnlyChoice = createChoiceItem("ReportSummariesOnly", localization);
-			ChoiceItem[] breakChoices = {
-				detailOnlyChoice,
-				detailAndBreaksChoice,
-				breaksOnlyChoice,
-			};
-			breakChoice = new UiComboBox(breakChoices);
-
-			if(savedBreakChoice == null)
-				breakChoice.setSelectedIndex(0);
-			else
-				breakChoice.setSelectedItem(savedBreakChoice);
-				
-			Box mainArea = Box.createVerticalBox();
-			mainArea.add(multiSortBox);
-			mainArea.add(breakChoice);
-			
-			contentPane.add(mainArea, BorderLayout.CENTER);
-			okButton = new UiButton(localization.getButtonLabel("ok"));
-			okButton.addActionListener(this);
-			UiButton cancelButton = new UiButton(localization.getButtonLabel("cancel"));
-			cancelButton.addActionListener(this);
-			Box buttonBar = Box.createHorizontalBox();
-			Component[] buttons = new Component[] {Box.createHorizontalGlue(), okButton, cancelButton};
-			Utilities.addComponentsRespectingOrientation(buttonBar, buttons);
-			contentPane.add(buttonBar, BorderLayout.AFTER_LAST_LINE);
-			getRootPane().setDefaultButton(okButton);
-			
-			pack();
-			Utilities.centerDlg(this);
-		}
-		
-		private ChoiceItem createChoiceItem(String tag, MiniLocalization localization)
-		{
-			return new ChoiceItem(tag, localization.getFieldLabel(tag));
-		}
-
-		private UiPopUpTreeEditor createSortChooser(UiMainWindow mainWindow, PopUpTreeFieldSpec spec)
-		{
-			UiLocalization localization = mainWindow.getLocalization();
-			UiPopUpTreeEditor chooser = new UiPopUpTreeEditor(localization);
-			chooser.setSpec(spec);
-			return chooser;
-		}
-		
-		void memorizeSortFields()
-		{			
-			sortMiniSpecs.clear();
-			for(int i = 0; i < sortChooser.length; ++i)
-			{
-				MiniFieldSpec spec = sortChooser[i].getSelectedMiniFieldSpec();
-				if(spec.getTag().length() == 0)
-					break;
-				sortMiniSpecs.add(spec);
-				
-				System.out.println("ActionMenuReport.getSortTags: " + spec);
-			}
-			savedBreakChoice = (ChoiceItem)breakChoice.getSelectedItem();
-		}
-		
-		public MiniFieldSpec[] getSelectedMiniFieldSpecs() throws Exception
-		{
-			return (MiniFieldSpec[])sortMiniSpecs.toArray(new MiniFieldSpec[0]);
-		}
-		
-		public boolean getPrintBreaks()
-		{
-			return !detailOnlyChoice.equals(breakChoice.getSelectedItem());
-		}
-		
-		public boolean getHideDetail()
-		{
-			return breaksOnlyChoice.equals(breakChoice.getSelectedItem());
-		}
-
-		public void actionPerformed(ActionEvent e)
-		{
-			if(e.getSource().equals(okButton))
-			{
-				memorizeSortFields();
-				hitOk = true;
-			}
-			dispose();
-		}
-		
-		public boolean ok()
-		{
-			return hitOk;
-		}
-
-		UiPopUpTreeEditor[] sortChooser;
-		boolean hitOk;
-		UiButton okButton;
-		UiComboBox breakChoice;
-		ChoiceItem detailOnlyChoice;
-		ChoiceItem detailAndBreaksChoice;
-		ChoiceItem breaksOnlyChoice;
-
-		private static Vector sortMiniSpecs;
-		private static ChoiceItem savedBreakChoice;
-	}
-
-	private static final int MAX_SORT_LEVELS = 3;
 }
 
---
+++ source/org/martus/client/swingui/dialogs/UiRunOrCreateReportDlg.java
@@ -0,0 +1,73 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2006, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.dialogs;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.JDialog;
+
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.swing.UiButton;
+import org.martus.swing.Utilities;
+
+public class UiRunOrCreateReportDlg extends JDialog implements ActionListener
+{
+	public UiRunOrCreateReportDlg(UiMainWindow mainWindow, String[] buttonLabels)
+	{
+		super(mainWindow);
+		setModal(true);
+		setTitle(mainWindow.getLocalization().getWindowTitle("RunOrCreateReport"));
+		Container panel = getContentPane();
+		Box buttonBox = Box.createVerticalBox();
+		for(int i = 0; i < buttonLabels.length; ++i)
+		{
+			UiButton button = new UiButton(buttonLabels[i]);
+			button.addActionListener(this);
+			buttonBox.add(button);
+		}
+		
+		panel.add(buttonBox);
+		pack();
+		Utilities.centerDlg(this);
+	}
+	
+	public String getPressedButtonLabel()
+	{
+		return pressedButtonLabel;
+	}
+
+	public void actionPerformed(ActionEvent event)
+	{
+		UiButton button = (UiButton)event.getSource();
+		pressedButtonLabel = button.getText();
+		dispose();
+	}
+	
+	String pressedButtonLabel;
+}
