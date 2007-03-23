d41e1564d51f58ae26158b28e1f6077a037652a1
kevins <kevins@localhost>
2007-03-23 20:26:23 +0000

    Eliminate warnings in previous commit

--- source/org/martus/client/bulletinstore/KnownFieldSpecCache.java
+++ source/org/martus/client/bulletinstore/KnownFieldSpecCache.java
@@ -30,7 +30,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -63,7 +62,6 @@ import org.martus.common.packet.UniversalId;
 import org.martus.common.packet.Packet.InvalidPacketException;
 import org.martus.common.packet.Packet.SignatureVerificationException;
 import org.martus.common.packet.Packet.WrongPacketTypeException;
-import org.martus.util.UnicodeWriter;
 import org.martus.util.inputstreamwithseek.InputStreamWithSeek;
 
 public class KnownFieldSpecCache extends BulletinStoreCache implements ReadableDatabase.PacketVisitor
