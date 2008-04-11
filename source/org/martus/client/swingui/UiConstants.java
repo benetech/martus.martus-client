8bc50200f6fbffca49bed7aba053e49c4f275960
kevins <kevins@localhost>
2008-04-11 16:38:19 +0000

    Update splash and about to copyright 2008

--- source/org/martus/client/swingui/UiConstants.java
+++ source/org/martus/client/swingui/UiConstants.java
@@ -34,7 +34,7 @@ public class UiConstants
 	public static final String programName = "Martus(TM)";
 	public static String versionLabel = "Pre-3.3-Internal";
 	public static final String copyrightSymbol = "\u00A9 "; 
-	public static final String copyright = copyrightSymbol + "Copyright 2001-2007, Beneficent Technology, Inc. (The Benetech Initiative)";
+	public static final String copyright = copyrightSymbol + "Copyright 2001-2008, Beneficent Technology, Inc. (The Benetech Initiative)";
 	public static final String website = "(Benetech, at www.benetech.org)";
 
 }
