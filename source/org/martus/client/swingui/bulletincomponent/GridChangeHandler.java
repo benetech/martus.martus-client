281de6437bd7bcba7351b5652d07314802115adf
kevins <kevins@localhost>
2010-09-24 23:36:52 +0000

    One more attempt to fix blanking-out (TT 4123)

--- source/org/martus/client/swingui/bulletincomponent/GridChangeHandler.java
+++ source/org/martus/client/swingui/bulletincomponent/GridChangeHandler.java
@@ -111,12 +111,11 @@ public class GridChangeHandler implements TableModelListener
 
 		// NOTE: Attempted fix for TT 4123, combos blank out 
 		// on Windows--may be Java6 optimization issue
-		Container parent = choiceField.getComponent().getParent();
+		Container parent = choiceField.getComponent().getTopLevelAncestor();
 		if(parent != null)
 		{
 			parent.invalidate();
 			parent.validate();
-			parent.repaint();
 		}
 
 	}
