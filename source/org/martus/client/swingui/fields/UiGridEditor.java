f176aedee9a1743311cfabc68cfcc336fbae143a
Kevin Smith <Kevin Smith@localhost>
2012-07-11 16:06:44 -0600

    Fix spellcheck in grids (in both grid and expanded form)

--- source/org/martus/client/swingui/fields/UiGridEditor.java
+++ source/org/martus/client/swingui/fields/UiGridEditor.java
@@ -108,4 +108,27 @@ public class UiGridEditor extends UiEditableGrid
 			throw e;
 		}
 	}
+	
+	@Override
+	public void updateSpellChecker(String bulletinLanguageCode)
+	{
+		super.updateSpellChecker(bulletinLanguageCode);
+		
+		if(expandedFieldRows != null)
+			updateSpellCheckerForExpandedFields(bulletinLanguageCode);
+	}
+
+	private void updateSpellCheckerForExpandedFields(String bulletinLanguageCode)
+	{
+		for(int row = 0; row < expandedFieldRows.size(); ++row)
+		{
+			UiField[] fields = (UiField[])expandedFieldRows.get(row);
+			for(int fieldIndex = 0; fieldIndex < fields.length; ++fieldIndex)
+			{
+				UiField field = fields[fieldIndex];
+				if(field != null)
+					field.updateSpellChecker(bulletinLanguageCode);
+			}
+		}
+	}
 }
