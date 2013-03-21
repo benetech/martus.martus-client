711d784dcee635e388b8fe430d836b468e6a9777
Rom Srinivasan <Rom Srinivasan@localhost>
2013-03-21 09:23:13 -0700

    HQ nicknames are now only displayed conditionally as Zawgyi otherwise they are in unicode

---
+++ source/org/martus/client/swingui/actions/ActionMenuExportMba.java
@@ -0,0 +1,94 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2006, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.actions;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Vector;
+
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.common.MartusLogger;
+import org.martus.common.bulletin.Bulletin;
+import org.martus.common.bulletin.BulletinZipUtilities;
+import org.martus.common.crypto.MartusCrypto.CryptoException;
+import org.martus.common.crypto.MartusCrypto.DecryptionException;
+import org.martus.common.crypto.MartusCrypto.NoKeyPairException;
+import org.martus.common.database.DatabaseKey;
+import org.martus.common.database.ReadableDatabase;
+import org.martus.common.database.Database.RecordHiddenException;
+import org.martus.common.packet.Packet.InvalidPacketException;
+import org.martus.common.packet.Packet.SignatureVerificationException;
+import org.martus.common.packet.Packet.WrongPacketTypeException;
+import org.martus.swing.UiFileChooser;
+import org.martus.swing.UiFileChooser.FileDialogResults;
+
+public class ActionMenuExportMba extends UiMenuAction
+{
+	public ActionMenuExportMba(UiMainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse, "ExportMBA");
+	}
+
+	public void actionPerformed(ActionEvent event)
+	{
+		doExportBulletinAsMba();
+	}
+
+	public void doExportBulletinAsMba()
+	{
+		try
+		{
+			Vector bulletins = mainWindow.getSelectedBulletins("ExportZeroBulletins");
+			if(bulletins == null)
+				return;
+			if(bulletins.size()!=1)
+				mainWindow.notifyDlg("ExportMbaSingleBulletinOnly");
+			String title = mainWindow.getLocalization().getWindowTitle("ExportMba");
+			Bulletin bulletin = (Bulletin)bulletins.get(0);
+			String defaultFileName = bulletin.toFileName();
+			FileDialogResults results = UiFileChooser.displayFileSaveDialog(mainWindow, title, defaultFileName);
+			if(results.wasCancelChoosen())
+				return;
+			File destination = results.getChosenFile();
+			exportBulletinToMba(bulletin, destination);
+		} 
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+			mainWindow.unexpectedErrorDlg();
+		}
+	}
+
+	private void exportBulletinToMba(Bulletin bulletin, File destination) throws UnsupportedEncodingException, InvalidPacketException, WrongPacketTypeException, SignatureVerificationException, DecryptionException, NoKeyPairException, FileNotFoundException, IOException, CryptoException, RecordHiddenException
+	{
+		ReadableDatabase db = mainWindow.getApp().getStore().getDatabase();
+		DatabaseKey headerKey = DatabaseKey.createKey(bulletin.getUniversalId(), bulletin.getStatus()); 
+		BulletinZipUtilities.exportBulletinPacketsFromDatabaseToZipFile(db, headerKey, destination, mainWindow.getApp().getSecurity());
+	}
+}
