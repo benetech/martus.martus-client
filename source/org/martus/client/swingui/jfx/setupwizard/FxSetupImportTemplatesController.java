d50368c73d7ffc69cbc6e91b4a135c94fcca38ad
Kevin Smith <Kevin Smith@localhost>
2014-02-24 13:13:09 -0800

    Fix JavaFX classpath hack to work with Java 8 (where jfx is already present)

---
+++ source/org/martus/client/swingui/jfx/setupwizard/FxSetupImportTemplatesController.java
@@ -0,0 +1,198 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.jfx.FxController;
+import org.martus.common.ContactKey;
+import org.martus.common.ContactKeys;
+import org.martus.common.MartusLogger;
+import org.martus.common.fieldspec.ChoiceItem;
+
+//FIXME this class is under development and is not tied to the wizard 
+public class FxSetupImportTemplatesController extends AbstractFxSetupWizardController implements Initializable
+{
+	public FxSetupImportTemplatesController(UiMainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+
+	@Override
+	public String getFxmlLocation()
+	{
+		return "setupwizard/SetupImportTemplate.fxml";
+	}
+
+	@Override
+	public FxController getNextControllerClassName()
+	{
+		return new FxSetupUsernamePasswordController(getMainWindow());
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		genericTemplatesComboBox.setItems(FXCollections.observableArrayList(getDateFormatChoices()));
+		customTemplatesComboBox.setItems(FXCollections.observableArrayList(getCustomDropdownChoices()));
+	} 
+	
+	private ObservableList<ChoiceItem> getCustomDropdownChoices()
+	{
+		Vector<ChoiceItem> choices = new Vector<ChoiceItem>();
+		choices.add(new ChoiceItem(IMPORTF_FROM_CONTACTS_CODE, "Import from My Contacts"));
+		choices.add(new ChoiceItem(IMPORT_FROM_NEW_CONTACT_CODE, "Import from New Contact"));
+		
+		return FXCollections.observableArrayList(choices);
+	}
+
+	private ObservableList<ChoiceItem> getDateFormatChoices()
+	{
+		Vector<ChoiceItem> choices = new Vector<ChoiceItem>();
+		choices.add(new ChoiceItem("template1", "Template 1"));
+		choices.add(new ChoiceItem("template2", "Template 2"));
+		choices.add(new ChoiceItem("template3", "Template 3"));
+
+		return FXCollections.observableArrayList(choices);
+	}
+	
+	@FXML
+	private void customDropDownSelectionChanged() throws Exception
+	{
+		String selectedCode = customTemplatesComboBox.getSelectionModel().getSelectedItem().getCode();
+		if (selectedCode.equals(IMPORTF_FROM_CONTACTS_CODE));
+			importFromContacts();
+		
+		if (selectedCode.equals(IMPORT_FROM_NEW_CONTACT_CODE))
+			importFromNewContact();
+	}
+	
+	private void importFromNewContact() throws Exception
+	{
+		showAddContactsDialog();
+	}
+
+	private void importFromContacts() throws Exception
+	{
+		showAddContactsDialog();
+	}
+	
+	private void showAddContactsDialog() throws Exception
+	{
+		showControllerInsideModalDialog(new ImportTemplatesFromMyContactsController(getMainWindow()), "Import Template");
+	}
+	
+	private static class ImportTemplatesFromMyContactsController extends FxController implements Initializable
+	{
+		public ImportTemplatesFromMyContactsController(UiMainWindow mainWindowToUse) throws Exception
+		{
+			super(mainWindowToUse);
+		}
+		
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1)
+		{
+			try
+			{
+				contactsWithTemplatesTableView.setEditable(true);
+				//getApp().attemptSignIn("c", "cccccccc".toCharArray());
+				//data.add(new ContactsWithTemplatesTableData("ABC", "1234", true, "dropDown"));
+				ContactKeys contactKey = getApp().getContactKeys();
+				for (int index = 0; index < contactKey.size(); ++index)
+				{
+					ContactKey key = contactKey.get(index);
+					data.add(new ContactsWithTemplatesTableData(key.getLabel(), key.getPublicCode(), false, ""));
+				}
+				
+				System.out.println("data size =" + data.size());
+			
+				contactSelectedColumn.setCellValueFactory(new PropertyValueFactory<ContactsWithTemplatesTableData, Boolean>("rowSelected"));
+				contactSelectedColumn.setCellFactory(new RadioButtonCellFactory());
+				
+				contactNameColumn.setCellValueFactory(new PropertyValueFactory<ContactsWithTemplatesTableData, String>("contactName"));
+				contactNameColumn.setCellFactory(TextFieldTableCell.<ContactsWithTemplatesTableData>forTableColumn());
+				
+				publicCodeColumn.setCellValueFactory(new PropertyValueFactory<ContactsWithTemplatesTableData, String>("publicCode"));
+				publicCodeColumn.setCellFactory(TextFieldTableCell.<ContactsWithTemplatesTableData>forTableColumn());
+
+				contactsWithTemplatesTableView.setItems(data);
+			}
+			catch (Exception e)
+			{
+				MartusLogger.logException(e);
+			}
+		}
+		
+		@Override
+		public String getFxmlLocation()
+		{
+			return "setupwizard/SetupImportTemplateFromMyContactsPopup.fxml";
+		}
+
+		@FXML
+		private TableView<ContactsWithTemplatesTableData> contactsWithTemplatesTableView;
+		
+		@FXML
+		private TableColumn<ContactsWithTemplatesTableData, String> contactNameColumn;
+		
+		@FXML
+		private TableColumn<ContactsWithTemplatesTableData, String> publicCodeColumn;
+		
+		@FXML
+		private TableColumn<ContactsWithTemplatesTableData, Boolean> contactSelectedColumn;
+		
+		private ObservableList<ContactsWithTemplatesTableData> data = FXCollections.observableArrayList();
+	}
+
+	@FXML
+	private ComboBox<ChoiceItem> genericTemplatesComboBox;
+	
+	@FXML
+	private ComboBox<ChoiceItem> customTemplatesComboBox;
+	
+	@FXML
+	private RadioButton genericRadioButton;
+	
+	@FXML
+	private RadioButton downloadCustomRadioButton;
+	
+	private static final String IMPORTF_FROM_CONTACTS_CODE = "importFromContacts";
+	private static final String IMPORT_FROM_NEW_CONTACT_CODE = "importFromNewContact";
+}
---
+++ source/org/martus/client/swingui/jfx/setupwizard/FxSetupUsernamePasswordController.java
@@ -0,0 +1,146 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+
+import org.martus.client.core.MartusUserNameAndPassword;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.jfx.FxController;
+
+public class FxSetupUsernamePasswordController extends AbstractFxSetupWizardController implements Initializable
+{
+	public FxSetupUsernamePasswordController(UiMainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1)
+	{
+		getWizardNavigationHandler().getNextButton().setDisable(true);
+		getUserName().textProperty().addListener(new LoginChangeHandler());
+		getPasswordField().textProperty().addListener(new LoginChangeHandler());
+		hintLabel.setTooltip(new Tooltip("Create secure passwords by using numbers, letters and sympbols."));
+	}
+
+	@Override
+	public void nextWasPressed(ActionEvent event) 
+	{
+		StaticAccountCreationData.setUserName(getUserName().getText());
+		StaticAccountCreationData.setPassword(getPasswordField().getText().toCharArray());
+	}
+	
+	private Label getErrorLabel()
+	{
+		return errorLabel;
+	}
+
+	public class LoginChangeHandler implements ChangeListener<String>
+	{
+		@Override
+		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
+		{
+			boolean canContinue = false;
+			if(getUserName().getText().length() > 0)
+			{
+				char[] password = getPasswordField().getText().toCharArray();
+				if(password.length >= MartusUserNameAndPassword.BASIC_PASSWORD_LENGTH)
+				{
+					canContinue = true;
+				}
+			}
+
+			updateErrorLabel(canContinue);
+			
+			getWizardNavigationHandler().getNextButton().setDisable(!canContinue);
+		}
+
+		private void updateErrorLabel(boolean canContinue)
+		{
+			String errorMessage = "";
+			if (!canContinue)
+				errorMessage = "Password must be at least 8 characters, 15 recommened.";
+			
+			getErrorLabel().setText(errorMessage);
+		}
+	}
+	
+	@Override
+	public String getFxmlLocation()
+	{
+		return "setupwizard/SetupUsernamePassword.fxml";
+	}
+	
+	@Override
+	public FxController getNextControllerClassName()
+	{
+		return new FxVerifyAccountController(getMainWindow());
+	}
+	
+	public PasswordField getPasswordField()
+	{
+		return passwordField;
+	}
+
+	public void setPasswordField(PasswordField passwordField)
+	{
+		this.passwordField = passwordField;
+	}
+
+	public TextField getUserName()
+	{
+		return userName;
+	}
+
+	public void setUserName(TextField userName)
+	{
+		this.userName = userName;
+	}
+
+	@FXML
+	private TextField userName;
+	
+	@FXML
+	private PasswordField passwordField;
+	
+	@FXML
+	private Label errorLabel;
+	
+	@FXML
+	private Label hintLabel;
+}
