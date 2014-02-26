6998c380f9c46590e320075dffa1f6a916b129d1
nimaa <nimaa@localhost>
2014-02-26 09:49:11 -0500

    Changed how lists are initialized and updated.

---
+++ source/org/martus/client/swingui/jfx/setupwizard/ContactsWithTemplatesTableData.java
@@ -0,0 +1,95 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
+
+import org.martus.common.fieldspec.ChoiceItem;
+
+public class ContactsWithTemplatesTableData
+{
+	public ContactsWithTemplatesTableData(String contactNameToUse, String publicCodeToUse, boolean rowSelectedToUse, ObservableList<ChoiceItem> selectedTemplateNameToUse)
+	{
+		contactName = new SimpleStringProperty(contactNameToUse);
+		publicCode = new SimpleStringProperty(publicCodeToUse);
+		rowSelected = new SimpleBooleanProperty(rowSelectedToUse);
+		selectedTemplateName = new SimpleListProperty<ChoiceItem>();
+		selectedTemplateName.addAll(selectedTemplateNameToUse);
+	}
+	
+	public String getContactName()
+	{
+		return contactName.get();
+	}
+	
+	public void setContactName(String contactNameToUse)
+	{
+		contactName.set(contactNameToUse);
+	}
+	
+	public String getPublicCode()
+	{
+		return publicCode.get();
+	}
+	
+	public void setPublicCode(String publicCodeToUse)
+	{
+		this.publicCode.set(publicCodeToUse);
+	}
+	
+	public boolean getRowSelected()
+	{
+		return rowSelected.get();
+	}
+	
+	public SimpleBooleanProperty getRowSelectedProperty()
+	{
+		return rowSelected;
+	}
+	
+	public void setRowSelected(boolean rowSelectedToUse)
+	{
+		rowSelected.set(rowSelectedToUse);
+	}
+	
+	public ObservableList<ChoiceItem> getSelectedTemplateName()
+	{
+		return selectedTemplateName.get();
+	}
+	
+	public void setSelectedTemplateName(ObservableList<ChoiceItem> selectedTamplateNameToUse)
+	{
+		selectedTemplateName.addAll(selectedTamplateNameToUse);
+	}
+	
+	private final SimpleStringProperty contactName;
+	private final SimpleStringProperty publicCode;
+	private final SimpleBooleanProperty rowSelected;
+	private final SimpleListProperty<ChoiceItem> selectedTemplateName;
+}
