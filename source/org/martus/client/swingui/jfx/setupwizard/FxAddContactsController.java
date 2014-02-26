f609b34fda5163f843347b32a89efce184388070
nimaa <nimaa@localhost>
2014-02-26 09:49:11 -0500

    Changed how lists are initialized and updated.

---
+++ source/org/martus/client/swingui/jfx/setupwizard/FxAddContactsController.java
@@ -0,0 +1,307 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+
+import org.martus.client.core.MartusApp;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.jfx.FxController;
+import org.martus.common.ContactKey;
+import org.martus.common.ContactKeys;
+import org.martus.common.Exceptions.ServerNotAvailableException;
+import org.martus.common.MartusAccountAccessToken;
+import org.martus.common.MartusAccountAccessToken.TokenNotFoundException;
+import org.martus.common.MartusLogger;
+import org.martus.common.crypto.MartusSecurity;
+
+public class FxAddContactsController extends AbstractFxSetupWizardContentController implements Initializable
+{
+	public FxAddContactsController(UiMainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		publicCodeColumn.setEditable(false);
+		
+		contactNameColumn.setCellValueFactory(new PropertyValueFactory<ContactsTableData, String>("contactName"));
+		publicCodeColumn.setCellValueFactory(new PropertyValueFactory<ContactsTableData, String>("publicCode"));
+		
+		contactNameColumn.setCellFactory(TextFieldTableCell.<ContactsTableData>forTableColumn());
+		publicCodeColumn.setCellFactory(TextFieldTableCell.<ContactsTableData>forTableColumn());
+		
+		contactsTable.setItems(data);
+		loadExistingContactData();
+
+		updateAddContactButtonState();
+		
+		accessTokenField.textProperty().addListener(new AccessTokenChangeHandler());
+	}
+	
+	@FXML
+	public void addContact() 
+	{
+		try
+		{
+			MartusAccountAccessToken token = new MartusAccountAccessToken(accessTokenField.getText());
+			MartusApp app = getApp();
+			String contactAccountId = app.getMartusAccountIdFromAccessTokenOnServer(token);
+			if(contactAccountId.equals(app.getAccountId()))
+			{
+				showNotifyDlg("ContactKeyIsOurself");
+				return;
+			}
+			String contactPublicCode = MartusSecurity.computeFormattedPublicCode(contactAccountId);
+			if(DoesContactAlreadyExistInTable(contactPublicCode))
+			{
+				showNotifyDlg("ContactKeyAlreadyExists");
+				return;
+			}
+			showAndAddContactsDialog(contactPublicCode);
+		} 
+		catch (ServerNotAvailableException e)
+		{
+			showNotifyDlg("ContactsNoServer");
+		} 
+		catch (TokenNotFoundException e)
+		{
+			showNotifyDlg("UnableToRetrieveContactFromServer");
+		} 
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+			showNotifyDlg("UnexpectedError");
+		} 
+	}
+
+	private boolean DoesContactAlreadyExistInTable(String contactPublicCode)
+	{
+		for(int i =0; i < data.size(); ++i)
+		{
+			ContactsTableData contactData = data.get(i);
+			if(contactData.getPublicCode().equals(contactPublicCode))
+			return true;
+		}
+		return false;
+	}
+	
+	private void showAndAddContactsDialog(String contactPublicCode)
+	{
+		try
+		{
+			PopupController popupController = new PopupController(getMainWindow(), contactPublicCode);
+			showControllerInsideModalDialog(popupController, "AddContact");
+			if(popupController.hasContactBeenAccepted())
+			{
+				int verification = popupController.getVerification();
+				String blankNameToStart = "";
+				boolean canSendTo = false;
+				boolean canReceiveFrom = false;
+				ContactsTableData contactData = new ContactsTableData(blankNameToStart, contactPublicCode, canSendTo, canReceiveFrom); 
+				data.add(contactData);
+				clearAccessTokenField();
+			}
+		}
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+		}
+	}
+
+	private void clearAccessTokenField()
+	{
+		accessTokenField.setText("");
+	}
+	
+	public static class PopupController extends FxController implements Initializable
+	{
+		public PopupController(UiMainWindow mainWindowToUse, String contactPublicCodeToUse)
+		{
+			super(mainWindowToUse);
+			contactPublicCode = contactPublicCodeToUse;
+			verification=ContactKey.NOT_VERIFIED;
+			contactAccepted = false;
+		}
+		
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1)
+		{
+			contactPublicCodeLabel.setText(contactPublicCode);
+		}
+		
+		@Override
+		public String getFxmlLocation()
+		{
+			return "setupwizard/SetupAddContactPopup.fxml";
+		}
+
+		@FXML
+		public void cancelVerify()
+		{
+			getStage().close();
+		}
+
+		@FXML
+		public void verifyContact()
+		{
+			verification=ContactKey.VERIFIED_VISUALLY;
+			contactAccepted = true;
+			getStage().close();
+		}
+
+		@FXML
+		private Label contactPublicCodeLabel;
+		
+		public int getVerification()
+		{
+			return verification;
+		}
+		
+		public boolean hasContactBeenAccepted()
+		{
+			return contactAccepted;
+		}
+		
+		private String contactPublicCode;
+		private int verification;
+		private boolean contactAccepted;
+	}
+	
+	@Override
+	public String getFxmlLocation()
+	{
+		return "setupwizard/SetupAddContacts.fxml";
+	}
+	
+	@Override
+	public FxController getNextControllerClassName()
+	{
+		return new FxSetupImportFormTemplates(getMainWindow());
+	}
+	
+	protected class AccessTokenChangeHandler implements ChangeListener<String>
+	{
+		@Override
+		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
+		{
+			updateAddContactButtonState();
+		}
+
+	}
+	
+	protected void updateAddContactButtonState()
+	{
+		String candidateToken = accessTokenField.getText();
+		boolean canAdd = (isValidAccessToken(candidateToken));
+
+		Button nextButton = getWizardNavigationHandler().getNextButton();
+		if(candidateToken.length() == 0)
+		{
+			addContactButton.setDefaultButton(false);
+			nextButton.setDefaultButton(true);
+		}
+		else if(canAdd)
+		{
+			nextButton.setDefaultButton(false);
+			addContactButton.setDefaultButton(true);
+		}
+		else
+		{
+			nextButton.setDefaultButton(false);
+			addContactButton.setDefaultButton(true);
+		}
+
+		addContactButton.setDisable(!canAdd);
+	}
+
+	private boolean isValidAccessToken(String tokenToValidate)
+	{
+		if(tokenToValidate.length() == 0)
+			return false;
+		
+		return MartusAccountAccessToken.isTokenValid(tokenToValidate);
+	}
+	
+	private void loadExistingContactData()
+	{
+		data.clear();
+		
+		try
+		{
+			ContactKeys keys = getApp().getContactKeys();
+			for(int i = 0; i < keys.size(); ++i)
+			{
+				ContactKey contact = keys.get(i);
+				String name = contact.getLabel();
+				String publicCode = contact.getPublicCode();
+				boolean canSendTo = contact.getCanSendTo();
+				boolean canReceiveFrom = contact.getCanReceiveFrom();
+				ContactsTableData contactData = new ContactsTableData(name, publicCode, canSendTo, canReceiveFrom); 
+				data.add(contactData);
+			}
+		} 
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+		}
+		
+	}
+
+	@FXML
+	private TableView<ContactsTableData> contactsTable;
+	
+	@FXML
+	private TableColumn<ContactsTableData, String> contactNameColumn;
+	
+	@FXML
+	private TableColumn<ContactsTableData, String> publicCodeColumn;
+	
+	@FXML
+	private TextField accessTokenField;
+	
+	@FXML
+	private Button addContactButton;
+	
+	private ObservableList<ContactsTableData> data = FXCollections.observableArrayList();
+}
---
+++ source/org/martus/client/swingui/jfx/setupwizard/FxSetupImportTemplatesController.java
@@ -0,0 +1,242 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.cell.ComboBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.jfx.FxController;
+import org.martus.common.ContactKey;
+import org.martus.common.ContactKeys;
+import org.martus.common.MartusLogger;
+import org.martus.common.fieldspec.ChoiceItem;
+
+//FIXME this class is under development and is not tied to the wizard 
+public class FxSetupImportTemplatesController extends AbstractFxSetupWizardContentController implements Initializable
+{
+	public FxSetupImportTemplatesController(UiMainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+
+	@Override
+	public String getFxmlLocation()
+	{
+		return "setupwizard/SetupImportTemplate.fxml";
+	}
+
+	@Override
+	public FxController getNextControllerClassName()
+	{
+		return new FxSetupUsernamePasswordController(getMainWindow());
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		genericTemplatesComboBox.setItems(FXCollections.observableArrayList(getDateFormatChoices()));
+		customTemplatesComboBox.setItems(FXCollections.observableArrayList(getCustomDropdownChoices()));
+	} 
+	
+	private ObservableList<ChoiceItem> getCustomDropdownChoices()
+	{
+		Vector<ChoiceItem> choices = new Vector<ChoiceItem>();
+		choices.add(new ChoiceItem(IMPORTF_FROM_CONTACTS_CODE, "Import from My Contacts"));
+		choices.add(new ChoiceItem(IMPORT_FROM_NEW_CONTACT_CODE, "Import from New Contact"));
+		
+		return FXCollections.observableArrayList(choices);
+	}
+
+	private ObservableList<ChoiceItem> getDateFormatChoices()
+	{
+		Vector<ChoiceItem> choices = new Vector<ChoiceItem>();
+		choices.add(new ChoiceItem("template1", "Template 1"));
+		choices.add(new ChoiceItem("template2", "Template 2"));
+		choices.add(new ChoiceItem("template3", "Template 3"));
+
+		return FXCollections.observableArrayList(choices);
+	}
+	
+	@FXML
+	private void customDropDownSelectionChanged() throws Exception
+	{
+		String selectedCode = customTemplatesComboBox.getSelectionModel().getSelectedItem().getCode();
+		if (selectedCode.equals(IMPORTF_FROM_CONTACTS_CODE));
+			importFromContacts();
+		
+		if (selectedCode.equals(IMPORT_FROM_NEW_CONTACT_CODE))
+			importFromNewContact();
+	}
+	
+	private void importFromNewContact() throws Exception
+	{
+		showAddContactsDialog();
+	}
+
+	private void importFromContacts() throws Exception
+	{
+		showAddContactsDialog();
+	}
+	
+	private void showAddContactsDialog() throws Exception
+	{
+		showControllerInsideModalDialog(new ImportTemplatesFromMyContactsController(getMainWindow()), "ImportTemplate");
+	}
+	
+	private static class ImportTemplatesFromMyContactsController extends FxController implements Initializable
+	{
+		public ImportTemplatesFromMyContactsController(UiMainWindow mainWindowToUse) throws Exception
+		{
+			super(mainWindowToUse);
+		}
+		
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1)
+		{
+			try
+			{
+				contactsWithTemplatesTableView.setEditable(true);
+				ContactKeys contactKey = getApp().getContactKeys();
+				for (int index = 0; index < contactKey.size(); ++index)
+				{
+					ContactKey key = contactKey.get(index);
+					
+					ObservableList<ChoiceItem> observableList = FXCollections.observableArrayList();
+					observableList.add(new ChoiceItem("", "Choose One..."));
+					observableList.add(new ChoiceItem("NewItem1", "New Item1"));
+					observableList.add(new ChoiceItem("NewItem2", "New Item2"));
+
+					ContactsWithTemplatesTableData e = new ContactsWithTemplatesTableData(key.getLabel(), key.getPublicCode(), true, observableList);
+					
+					data.add(e);
+				}
+				
+				System.out.println("data size =" + data.size());
+			
+				contactSelectedColumn.setCellValueFactory(new PropertyValueFactory<ContactsWithTemplatesTableData, Boolean>("rowSelected"));
+				contactSelectedColumn.setCellFactory(new RadioButtonCellFactory(new ToggleChangeListener(contactsWithTemplatesTableView)));
+				
+				contactNameColumn.setCellValueFactory(new PropertyValueFactory<ContactsWithTemplatesTableData, String>("contactName"));
+				contactNameColumn.setCellFactory(TextFieldTableCell.<ContactsWithTemplatesTableData>forTableColumn());
+				
+				publicCodeColumn.setCellValueFactory(new PropertyValueFactory<ContactsWithTemplatesTableData, String>("publicCode"));
+				publicCodeColumn.setCellFactory(TextFieldTableCell.<ContactsWithTemplatesTableData>forTableColumn());
+				
+				selectedTemplateColumn.setCellValueFactory(new PropertyValueFactory<ContactsWithTemplatesTableData, ChoiceItem>("selectedTemplateName"));
+				selectedTemplateColumn.setCellFactory(ComboBoxTableCell.<ContactsWithTemplatesTableData, ChoiceItem>forTableColumn());
+				
+				contactsWithTemplatesTableView.setItems(data);
+			}
+			catch (Exception e)
+			{
+				MartusLogger.logException(e);
+			}
+		}
+		
+		@Override
+		public String getFxmlLocation()
+		{
+			return "setupwizard/SetupImportTemplateFromMyContactsPopup.fxml";
+		}
+
+		@FXML
+		private TableView<ContactsWithTemplatesTableData> contactsWithTemplatesTableView;
+		
+		@FXML
+		private TableColumn<ContactsWithTemplatesTableData, String> contactNameColumn;
+		
+		@FXML
+		private TableColumn<ContactsWithTemplatesTableData, String> publicCodeColumn;
+		
+		@FXML
+		private TableColumn<ContactsWithTemplatesTableData, Boolean> contactSelectedColumn;
+		
+		@FXML
+		private TableColumn<ContactsWithTemplatesTableData, ChoiceItem> selectedTemplateColumn;
+		
+		private ObservableList<ContactsWithTemplatesTableData> data = FXCollections.observableArrayList();
+	}
+	
+	private static class ToggleChangeListener implements ChangeListener<Toggle>
+	{
+		public ToggleChangeListener(TableView<ContactsWithTemplatesTableData> contactsWithTemplatesTableViewToUse)
+		{
+			contactsWithTemplatesTableView = contactsWithTemplatesTableViewToUse;
+		}
+
+		@Override
+		public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue)
+		{
+			if (contactsWithTemplatesTableView.getSelectionModel().isEmpty())
+				return;
+			
+			ContactsWithTemplatesTableData dataForRow = contactsWithTemplatesTableView.getSelectionModel().getSelectedItem();
+			dataForRow.setSelectedTemplateName(getAllTemplatesForContact());
+		}
+		
+		private ObservableList<ChoiceItem> getAllTemplatesForContact()
+		{
+			ObservableList<ChoiceItem> observableArrayList = FXCollections.observableArrayList();
+			//ADD contact templates here.  
+
+			return 	observableArrayList;
+		}
+
+		private TableView<ContactsWithTemplatesTableData> contactsWithTemplatesTableView;
+	}
+	
+	@FXML
+	private ComboBox<ChoiceItem> genericTemplatesComboBox;
+	
+	@FXML
+	private ComboBox<ChoiceItem> customTemplatesComboBox;
+	
+	@FXML
+	private RadioButton genericRadioButton;
+	
+	@FXML
+	private RadioButton downloadCustomRadioButton;
+	
+	private static final String IMPORTF_FROM_CONTACTS_CODE = "importFromContacts";
+	private static final String IMPORT_FROM_NEW_CONTACT_CODE = "importFromNewContact";
+}
