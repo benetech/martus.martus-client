f83b3dbf57152c678d4de238d3d2a43869638e25
Kevin Smith <Kevin Smith@localhost>
2014-02-26 15:54:16 -0800

    Prevent continuing from advanced storage unless server worked

---
+++ source/org/martus/client/swingui/jfx/setupwizard/ContactsTableData.java
@@ -0,0 +1,110 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import org.martus.common.ContactKey;
+import org.martus.util.StreamableBase64.InvalidBase64Exception;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class ContactsTableData
+{
+	public ContactsTableData(ContactKey contact) throws InvalidBase64Exception
+	{
+		contactName = new SimpleStringProperty(contact.getLabel());
+		publicCode = new SimpleStringProperty(contact.getPublicCode());
+		canSendTo = new SimpleBooleanProperty(contact.getCanSendTo());
+		canReceiveFrom = new SimpleBooleanProperty(contact.getCanReceiveFrom());
+		removeContact = new SimpleStringProperty("X");
+	}
+	
+	public String getContactName()
+	{
+		return contactName.get();
+	}
+	
+	public void setContactName(String contactNameToUse)
+	{
+		contactName.set(contactNameToUse);
+	}
+	
+	public String getDeleteContact()
+	{
+		return removeContact.get();
+	}
+	
+	public void setDeleteContact(String removeContactToUse)
+	{
+		removeContact.set(removeContactToUse);
+	}
+
+	public String getPublicCode()
+	{
+		return publicCode.get();
+	}
+	
+	public void setPublicCode(String publicCodeToUse)
+	{
+		this.publicCode.set(publicCodeToUse);
+	}
+	
+	public boolean getCanSendTo()
+	{
+		return canSendTo.get();
+	}
+	
+	public void setCanSendTo(boolean canSendToToUse)
+	{
+		canSendTo.set(canSendToToUse);
+	}
+	
+	public boolean getCanReceiveFrom()
+	{
+		return canReceiveFrom.get();
+	}
+	
+	public void setCanReceiveFrom(boolean canReceiveFromToUse)
+	{
+		canReceiveFrom.set(canReceiveFromToUse);
+	}
+	
+    public SimpleBooleanProperty canSendToProperty() 
+    {
+    		return canSendTo;
+    }
+	
+    public SimpleBooleanProperty canReceiveFromProperty() 
+    {
+    		return canReceiveFrom;
+    }
+
+    private final SimpleStringProperty contactName;
+	private final SimpleStringProperty publicCode;
+	private final SimpleBooleanProperty canSendTo;
+	private final SimpleBooleanProperty canReceiveFrom;
+	private final SimpleStringProperty removeContact;
+}
---
+++ source/org/martus/client/swingui/jfx/setupwizard/FxSetupImportTemplatesController.java
@@ -0,0 +1,141 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.jfx.FxController;
+import org.martus.common.fieldspec.ChoiceItem;
+
+public class FxSetupImportTemplatesController extends AbstractFxSetupWizardContentController implements Initializable
+{
+	public FxSetupImportTemplatesController(UiMainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+
+	@Override
+	public String getFxmlLocation()
+	{
+		return "setupwizard/SetupImportTemplate.fxml";
+	}
+
+	@Override
+	public FxController getNextControllerClassName()
+	{
+		return new FxSetupBackupYourKeyController(getMainWindow());
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		genericTemplatesComboBox.setItems(FXCollections.observableArrayList(getGenericTemplateChoices()));
+		genericTemplatesComboBox.getSelectionModel().selectFirst();
+		
+		customTemplatesComboBox.setItems(FXCollections.observableArrayList(getImportTemplateChoices()));
+		customTemplatesComboBox.getSelectionModel().selectFirst();
+		
+		genericTemplatesComboBox.setVisible(false);
+		customTemplatesComboBox.setVisible(false);
+	} 
+	
+	private ObservableList<ChoiceItem> getImportTemplateChoices()
+	{
+		Vector<ChoiceItem> choices = new Vector<ChoiceItem>();
+		choices.add(new ChoiceItem("", "Choose One..."));
+		choices.add(new ChoiceItem(IMPORT_FROM_CONTACTS_CODE, "Import from My Contacts"));
+		choices.add(new ChoiceItem(IMPORT_FROM_NEW_CONTACT_CODE, "Import from New Contact"));
+		
+		return FXCollections.observableArrayList(choices);
+	}
+
+	private ObservableList<ChoiceItem> getGenericTemplateChoices()
+	{
+		Vector<ChoiceItem> choices = new Vector<ChoiceItem>();
+		choices.add(new ChoiceItem("", "Choose a form template"));
+		choices.add(new ChoiceItem("template2", "Template 2"));
+
+		return FXCollections.observableArrayList(choices);
+	}
+	
+	@FXML
+	private void customDropDownSelectionChanged() throws Exception
+	{
+		String selectedCode = customTemplatesComboBox.getSelectionModel().getSelectedItem().getCode();
+		if (selectedCode.equals(IMPORT_FROM_CONTACTS_CODE))
+			importFromContacts();
+		
+		if (selectedCode.equals(IMPORT_FROM_NEW_CONTACT_CODE))
+			importFromNewContact();
+	}
+	
+	@FXML
+	private void radioButtonSelectionChanged()
+	{
+		genericTemplatesComboBox.setVisible(false);
+		customTemplatesComboBox.setVisible(false);
+		if (genericRadioButton.isSelected())
+			genericTemplatesComboBox.setVisible(true);
+		
+		if (downloadCustomRadioButton.isSelected())
+			customTemplatesComboBox.setVisible(true);
+	}
+	
+	private void importFromNewContact() throws Exception
+	{
+		showControllerInsideModalDialog(new FxSetupTemplateFromNewContactPopupController(getMainWindow()), "ImportTemplate");
+	}
+
+	private void importFromContacts() throws Exception
+	{
+		showControllerInsideModalDialog(new FxImportTemplateFromMyContactsPopupController(getMainWindow()), "ImportTemplate");
+	}
+	
+	@FXML
+	private ComboBox<ChoiceItem> genericTemplatesComboBox;
+	
+	@FXML
+	private ComboBox<ChoiceItem> customTemplatesComboBox;
+	
+	@FXML
+	private RadioButton genericRadioButton;
+	
+	@FXML
+	private RadioButton downloadCustomRadioButton;
+	
+	private static final String IMPORT_FROM_CONTACTS_CODE = "importFromContacts";
+	private static final String IMPORT_FROM_NEW_CONTACT_CODE = "importFromNewContact";
+}
