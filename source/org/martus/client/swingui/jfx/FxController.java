a8c928a1fa0a8d9fa45184f168c425bfa559b1ea
Kevin Smith <Kevin Smith@localhost>
2014-03-05 13:05:12 -0800

    Extract static helper method that could be called without having an actual app

---
+++ source/org/martus/client/swingui/jfx/FxController.java
@@ -0,0 +1,272 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import org.martus.client.core.MartusApp;
+import org.martus.client.swingui.MartusLocalization;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.common.MartusLogger;
+
+abstract public class FxController
+{
+	public FxController(UiMainWindow mainWindowToUse)
+	{
+		mainWindow = mainWindowToUse;
+	}
+	
+	abstract public String getFxmlLocation();
+	
+	public Parent createContents() throws Exception
+	{
+		return (Parent)createLoader().load();
+	}
+	
+	private FxmlLoaderWithController createLoader() throws Exception
+	{
+		URL resourceUrl = getBestFxmlLocation();
+		
+		return new FxmlLoaderWithController(this, resourceUrl);
+	}
+
+	private URL getBestFxmlLocation() throws Exception
+	{
+		File fxmlDir = getApp().getFxmlDirectory();
+		return getBestFile(fxmlDir, getFxmlLocation());
+	}
+
+	public static URL getBestFile(File fxmlDir, String fileLocation) throws MalformedURLException
+	{
+		File fxmlFile = new File(fxmlDir, fileLocation);
+		if (fxmlFile.exists())
+		{
+			MartusLogger.log("Loading FX file from disk:" + fileLocation);
+			return fxmlFile.toURI().toURL();
+		}
+
+		return FxScene.class.getResource(fileLocation);
+	}		
+	
+	public UiMainWindow getMainWindow()
+	{
+		return mainWindow;
+	}
+	
+	public MartusLocalization getLocalization()
+	{
+		return getMainWindow().getLocalization();
+	}
+	
+	public MartusApp getApp()
+	{
+		return getMainWindow().getApp();
+	}
+	
+	public void showNotifyDlg(String baseTag)
+	{
+		try
+		{
+			PopupNotifyController popupController = new PopupNotifyController(getMainWindow(), baseTag);
+			showControllerInsideModalDialog(popupController);
+		} 
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+		}
+	}
+	
+	public boolean showConfirmationDlg(String title, String message)
+	{
+		try
+		{
+			PopupConfirmationController popupController = new PopupConfirmationController(getMainWindow(), title, message);
+			showControllerInsideModalDialog(popupController);
+			return popupController.wasYesPressed();
+		} 
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+		}
+		return false;
+	}
+
+	public static class PopupNotifyController extends FxPopupController implements Initializable
+	{
+		public PopupNotifyController(UiMainWindow mainWindowToUse, String notificationTag)
+		{
+			super(mainWindowToUse);
+			baseTag = notificationTag;
+		}
+		
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1)
+		{
+			MartusLocalization localization = getLocalization();
+			fxOkButton.setText(localization.getButtonLabel("ok"));
+			fxLabel.setText(localization.getFieldLabel("notify"+baseTag+"cause"));
+		}
+		
+		@Override
+		public String getFxmlLocation()
+		{
+			return "setupwizard/NotifyPopup.fxml";
+		}
+
+		@Override
+		public String getDialogTitle()
+		{
+			return getLocalization().getWindowTitle("notify"+ baseTag); 
+		}
+
+		@FXML
+		public void okPressed()
+		{
+			getStage().close();
+		}
+
+		public void setFxStage(FxInSwingDialogStage stageToUse)
+		{
+			fxStage = stageToUse;
+		}
+
+		public FxInSwingDialogStage getFxStage()
+		{
+			return fxStage;
+		}
+
+		@FXML
+		private Label fxLabel;
+		private FxInSwingDialogStage fxStage;
+		@FXML
+		private Button fxOkButton;
+		private String baseTag;
+	}
+
+	public static class PopupConfirmationController extends FxPopupController implements Initializable
+	{
+		public PopupConfirmationController(UiMainWindow mainWindowToUse, String title, String message)
+		{
+			super(mainWindowToUse);
+			this.title = title;
+			this.message = message;
+		}
+		
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1)
+		{
+			MartusLocalization localization = getLocalization();
+			fxYesButton.setText(localization.getButtonLabel("yes"));
+			fxNoButton.setText(localization.getButtonLabel("no"));
+			fxLabel.setText(message);
+		}
+		
+		@Override
+		public String getFxmlLocation()
+		{
+			return "setupwizard/ConfirmationPopup.fxml";
+		}
+
+		@Override
+		public String getDialogTitle()
+		{
+			return title; 
+		}
+
+		@FXML
+		public void yesPressed()
+		{
+			yesWasPressed = true;
+			getStage().close();
+		}
+
+		@FXML
+		public void noPressed()
+		{
+			getStage().close();
+		}
+
+		public void setFxStage(FxInSwingDialogStage stageToUse)
+		{
+			fxStage = stageToUse;
+		}
+
+		public FxInSwingDialogStage getFxStage()
+		{
+			return fxStage;
+		}
+		
+		public boolean wasYesPressed()
+		{
+			return yesWasPressed;
+		}
+
+		@FXML
+		private Label fxLabel;
+		private FxInSwingDialogStage fxStage;
+		@FXML
+		private Button fxYesButton;
+		@FXML
+		private Button fxNoButton;
+		
+		private String title;
+		private String message;
+		private boolean yesWasPressed;
+	}
+	
+	public void showControllerInsideModalDialog(FxPopupController controller) throws Exception
+	{
+		Stage popupStage = new Stage();
+		controller.setStage(popupStage);
+		popupStage.setTitle(controller.getDialogTitle());
+		popupStage.initModality(Modality.WINDOW_MODAL);
+
+		FXMLLoader fl = new FXMLLoader();
+		fl.setController(controller);
+		fl.setLocation(FxInSwingDialogStage.class.getResource(controller.getFxmlLocation()));
+		fl.load();
+		Parent root = fl.getRoot();
+
+		Scene scene = new Scene(root);
+		popupStage.setScene(scene);
+	    popupStage.showAndWait();
+	}
+
+	private UiMainWindow mainWindow;
+}
---
+++ source/org/martus/client/swingui/jfx/FxTableCellTextFieldFactory.java
@@ -0,0 +1,94 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+//Modified: original from http://stackoverflow.com/questions/7880494/tableview-better-editing-through-binding
+	
+package org.martus.client.swingui.jfx;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.util.Callback;
+
+public final class FxTableCellTextFieldFactory  
+	implements Callback<TableColumn<Object,String>,TableCell<Object,String>> 
+{
+	
+	@Override
+	public TableCell<Object, String> call(TableColumn<Object, String> param) 
+	{
+	   TextFieldCell textFieldCell = new TextFieldCell();
+	   return textFieldCell;
+	}
+	
+	public static class TextFieldCell extends TableCell<Object,String> 
+	{
+		public TextFieldCell() 
+		{
+			textField = new TextField();
+			this.setGraphic(textField);
+		}
+	   
+		@Override
+		protected void updateItem(String item, boolean empty) 
+		{
+			super.updateItem(item, empty);        
+			if(empty)
+				this.setContentDisplay(ContentDisplay.TEXT_ONLY);
+			else
+				bindToOurStringPropertyTextField();
+		}
+
+		private void bindToOurStringPropertyTextField()
+		{
+			this.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+			//NOTE: To use this TextField Factory the TableData's SimpleStringProperty must be implemented.  
+			//		IE: public SimpleStringProperty <variableName>Property() { return <variableName>; }
+			ObservableValue<String> cellObservableValue = getTableColumn().getCellObservableValue(getIndex());
+			SimpleStringProperty cellStringProperty = (SimpleStringProperty)cellObservableValue;
+	    
+			if(this.cellStringPropertyBoundToCurrently==null) 
+			{
+				this.cellStringPropertyBoundToCurrently = cellStringProperty;
+				this.textField.textProperty().bindBidirectional(cellStringProperty);
+			}
+			else
+			{
+				if(this.cellStringPropertyBoundToCurrently != cellStringProperty) 
+				{
+					this.textField.textProperty().unbindBidirectional(this.cellStringPropertyBoundToCurrently);
+					this.cellStringPropertyBoundToCurrently = cellStringProperty;
+					this.textField.textProperty().bindBidirectional(this.cellStringPropertyBoundToCurrently);
+				}
+			}
+		}
+		private TextField textField;
+		private StringProperty cellStringPropertyBoundToCurrently;
+	}
+}
