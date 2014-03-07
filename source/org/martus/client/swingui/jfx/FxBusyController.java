759eeaa0eaab8f761b126de33be9de9bebd3bc07
nimaa <nimaa@localhost>
2014-03-07 15:56:45 -0500

    Fixed combobox not saving selected value in table data by binding combo selected value to data.

---
+++ source/org/martus/client/swingui/jfx/FxBusyController.java
@@ -0,0 +1,89 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker.State;
+
+import org.martus.client.swingui.UiMainWindow;
+
+public class FxBusyController extends FxBackgroundActivityController
+{
+	public FxBusyController(UiMainWindow mainWindowToUse, String titleToUse, String messageToUse, Task taskToUse)
+	{
+		super(mainWindowToUse, titleToUse, messageToUse);
+		task = taskToUse;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle bundle)
+	{
+		super.initialize(location, bundle);
+		cancelButton.setVisible(false);
+		updateProgressBar(INDETERMINATE);
+		task.stateProperty().addListener(new TaskStateChangeHandler());
+
+		Thread thread = new Thread(task);
+		thread.setDaemon(false);
+		thread.start();
+	}
+	
+	protected class TaskStateChangeHandler implements ChangeListener<Task.State>
+	{
+		@Override
+		public void changed(ObservableValue<? extends State> observable, State oldState, State newState)
+		{
+			if(newState.equals(State.SUCCEEDED))
+			{
+				getStage().close();
+			}
+			else if(newState.equals(State.FAILED))
+			{
+				setThrownException(task.getException());
+				getStage().close();
+			}
+		}
+	}
+	
+	@Override
+	public void cancelPressed()
+	{
+	}
+
+	@Override
+	public String getFxmlLocation()
+	{
+		return "FxBusy.fxml";
+	}
+	
+	private static final double INDETERMINATE = -1.0;
+	protected Task task;
+}
---
+++ source/org/martus/client/swingui/jfx/setupwizard/FxSetupWizardAbstractServerSetupController.java
@@ -0,0 +1,134 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.setupwizard;
+
+import org.martus.client.core.ConfigInfo;
+import org.martus.client.core.MartusApp;
+import org.martus.client.core.MartusApp.SaveConfigInfoException;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.clientside.ClientSideNetworkGateway;
+import org.martus.common.MartusLogger;
+
+abstract public class FxSetupWizardAbstractServerSetupController extends AbstractFxSetupWizardContentController
+{
+	public FxSetupWizardAbstractServerSetupController(UiMainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+
+	public boolean attemptToConnect(String serverIPAddress, String serverPublicKey, boolean askComplianceAcceptance)
+	{
+		MartusLogger.log("Attempting to connect to: " + serverIPAddress);
+		MartusApp app = getApp();
+		try
+		{
+			getMainWindow().clearStatusMessage();
+			ClientSideNetworkGateway gateway = ClientSideNetworkGateway.buildGateway(serverIPAddress, serverPublicKey, getApp().getTransport());
+			
+			if(!getApp().isSSLServerAvailable(gateway))
+			{
+				// FIXME: This should be a confirmation
+				showNotifyDialog("ServerSSLNotResponding");
+				saveServerConfig(serverIPAddress, serverPublicKey, "");
+				return true;
+			}
+
+			String complianceStatement = getServerCompliance(gateway);
+			if(askComplianceAcceptance)
+			{
+				if(complianceStatement.equals(""))
+				{
+					showNotifyDialog("ServerComplianceFailed");
+					saveServerConfig(serverIPAddress, serverPublicKey, "");
+					return true;
+				}
+				
+				if(!acceptCompliance(complianceStatement))
+				{
+					ConfigInfo previousServerInfo = getApp().getConfigInfo();
+
+					//TODO:The following line shouldn't be necessary but without it, the trustmanager 
+					//will reject the old server, we don't know why.
+					ClientSideNetworkGateway.buildGateway(previousServerInfo.getServerName(), previousServerInfo.getServerPublicKey(), getApp().getTransport());
+					
+					if(serverIPAddress.equals(previousServerInfo.getServerName()) &&
+					   serverPublicKey.equals(previousServerInfo.getServerPublicKey()))
+					{
+						getApp().setServerInfo("","","");
+					}
+
+					return false;
+				}
+			}
+
+			getApp().setServerInfo(serverIPAddress, serverPublicKey, complianceStatement);
+			
+			app.getStore().clearOnServerLists();
+			
+			getMainWindow().forceRecheckOfUidsOnServer();
+			app.getStore().clearOnServerLists();
+			getMainWindow().repaint();
+			getMainWindow().setStatusMessageReady();
+			return true;
+		}
+		catch(SaveConfigInfoException e)
+		{
+			MartusLogger.logException(e);
+			showNotifyDialog("ErrorSavingConfig");
+			return false;
+		}
+	}
+	
+	private String getServerCompliance(ClientSideNetworkGateway gateway)
+	{
+		try
+		{
+			return getApp().getServerCompliance(gateway);
+		}
+		catch (Exception e)
+		{
+			return "";
+		}
+	}
+
+	private boolean acceptCompliance(String newServerCompliance)
+	{
+		// FIXME: Actually allow the user to accept/reject
+		showNotifyDialog("ReplaceThisWithAConfirmationDialogShowingTheComplianceStatement");
+		boolean accepted = true;
+		if(!accepted)
+			showNotifyDialog("UserRejectedServerCompliance");
+		
+		return accepted;
+	}
+
+	
+	private void saveServerConfig(String serverIPAddress, String serverPublicKey, String complianceStatement) throws SaveConfigInfoException
+	{
+		getApp().setServerInfo(serverIPAddress, serverPublicKey, complianceStatement);
+	}
+
+}
