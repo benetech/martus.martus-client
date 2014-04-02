d778a0b08d236df6f93a4eaae2ce35065ebe131d
charlesl <charlesl@localhost>
2014-04-02 09:15:30 -0700

    fixes MARTUSDEV-141 Tor checkbox should be an on/off slider button This is a quick hack, with embedded strings / color styling. using a Pane to add a new FxSwitchButton

---
+++ source/org/martus/client/swingui/dialogs/UiConfigServerDlg.java
@@ -0,0 +1,220 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2001-2007, Beneficent
+Technology, Inc. (The Benetech Initiative).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+
+package org.martus.client.swingui.dialogs;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+
+import org.martus.client.core.ConfigInfo;
+import org.martus.client.core.MartusApp;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.jfx.setupwizard.step3.FxSetupStorageServerController;
+import org.martus.clientside.UiLocalization;
+import org.martus.common.MartusLogger;
+import org.martus.common.crypto.MartusCrypto;
+import org.martus.swing.UiButton;
+import org.martus.swing.UiLabel;
+import org.martus.swing.UiParagraphPanel;
+import org.martus.swing.UiTextField;
+import org.martus.swing.Utilities;
+
+public class UiConfigServerDlg extends JDialog implements ActionListener
+{
+	public UiConfigServerDlg(UiMainWindow owner, ConfigInfo infoToUse)
+	{
+		super(owner, "", true);
+
+		info = infoToUse;
+		mainWindow = owner;
+		app = owner.getApp();
+		UiLocalization localization = mainWindow.getLocalization();
+		
+		setTitle(localization.getWindowTitle("ConfigServer"));
+		fieldIPAddress = new UiTextField(25);
+		fieldPublicCode = new UiTextField(40);
+
+		UiParagraphPanel panel = new UiParagraphPanel();
+		
+		UiLabel defaultServerLabel = new UiLabel(localization.getFieldLabel("ChooseDefaultServer"));
+		UiButton defaultButton = new UiButton(new ActionDefaultServer());
+		UiLabel description = new UiLabel(localization.getFieldLabel("DefaultServerDescription"));
+		Box hbox = Box.createHorizontalBox();
+		Utilities.addComponentsRespectingOrientation(hbox, new Component[] {defaultButton, description});
+		panel.addComponents(defaultServerLabel, hbox);
+		
+		
+		panel.addComponents(new UiLabel(localization.getFieldLabel("ServerNameEntry")), fieldIPAddress);
+		panel.addComponents(new UiLabel(localization.getFieldLabel("ServerPublicCodeEntry")), fieldPublicCode);
+		serverIPAddress = info.getServerName();
+		serverPublicKey = info.getServerPublicKey();
+
+		updateTextFields();
+
+		fieldIPAddress.requestFocus();
+
+		panel.addBlankLine();
+
+		ok = new UiButton(localization.getButtonLabel("ok"));
+		ok.addActionListener(this);
+		JButton cancel = new UiButton(localization.getButtonLabel("cancel"));
+		cancel.addActionListener(this);
+		panel.addComponents(ok, cancel);
+
+		getContentPane().add(panel);
+		getRootPane().setDefaultButton(ok);
+		Utilities.centerDlg(this);
+		setResizable(true);
+		setVisible(true);
+	}
+
+	public void updateTextFields()
+	{
+		fieldIPAddress.setText(serverIPAddress);
+
+		String knownServerPublicCode = "";
+		try
+		{
+			if(serverPublicKey.length() > 0)
+				knownServerPublicCode = MartusCrypto.computeFormattedPublicCode40(serverPublicKey);
+		}
+		catch (Exception e)
+		{
+		}
+		fieldPublicCode.setText(knownServerPublicCode);
+	}
+
+	public boolean getResult()
+	{
+		return result;
+	}
+
+	public String getServerIPAddress()
+	{
+		return serverIPAddress;
+	}
+
+	public String getServerPublicKey()
+	{
+		return serverPublicKey;
+	}
+
+	public void actionPerformed(ActionEvent ae)
+	{
+		result = false;
+		if(ae.getSource() == ok)
+			onOk();
+		else
+			dispose();
+	}
+	
+	protected void onOk()
+	{
+		String name = fieldIPAddress.getText();
+		String publicCode = fieldPublicCode.getText();
+		if(!ValidateInformation(name, publicCode))
+			return;
+		
+		result = true;
+		dispose();
+	}
+
+	private boolean ValidateInformation(String serverName, String userEnteredPublicCode)
+	{
+		if(serverName.length() == 0)
+			return errorMessage("InvalidServerName");
+
+		String normalizedPublicCode = MartusCrypto.removeNonDigits(userEnteredPublicCode);
+		if(normalizedPublicCode.length() == 0)
+			return errorMessage("InvalidServerCode");
+
+		String serverKey = null;
+		try
+		{
+			if(!app.isNonSSLServerAvailable(serverName))
+				return errorMessage("ConfigNoServer");
+
+			serverKey = app.getServerPublicKey(serverName);
+			String serverPublicCode = MartusCrypto.computePublicCode(serverKey);
+			String serverPublicCode40 = MartusCrypto.computePublicCode40(serverKey);
+			if(!(serverPublicCode.equals(normalizedPublicCode)||serverPublicCode40.equals(normalizedPublicCode)))
+				return errorMessage("ServerCodeWrong");
+		}
+		catch(Exception e)
+		{
+			MartusLogger.logException(e);
+			return errorMessage("ServerInfoInvalid");
+		}
+
+		serverIPAddress = serverName;
+		serverPublicKey = serverKey;
+		return true;
+	}
+
+	private boolean errorMessage(String messageTag)
+	{
+		mainWindow.notifyDlg(messageTag);
+		return false;
+	}
+	
+	protected class ActionDefaultServer extends AbstractAction
+	{
+		public ActionDefaultServer()
+		{
+			super(mainWindow.getLocalization().getButtonLabel("ChooseDefaultServer"));
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			serverIPAddress = FxSetupStorageServerController.IP_FOR_SL1_IE;
+			serverPublicKey = FxSetupStorageServerController.PUBLIC_KEY_FOR_SL1_IE;
+			updateTextFields();
+			onOk();
+		}
+		
+	}
+
+
+	MartusApp app;
+	UiMainWindow mainWindow;
+	ConfigInfo info;
+
+	JButton ok;
+	UiTextField fieldIPAddress;
+	UiTextField fieldPublicCode;
+
+	String serverIPAddress;
+	String serverPublicKey;
+
+	boolean result;
+}
