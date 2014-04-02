d372a62f835c5af2452d89a5145c7d2d3a3aabb8
charlesl <charlesl@localhost>
2014-04-02 09:15:30 -0700

    fixes MARTUSDEV-141 Tor checkbox should be an on/off slider button This is a quick hack, with embedded strings / color styling. using a Pane to add a new FxSwitchButton

---
+++ source/org/martus/client/swingui/jfx/FxSwitchButton.java
@@ -0,0 +1,105 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+/* 
+ Original code from 
+ http://stackoverflow.com/questions/17467137/how-can-i-create-a-switch-button-in-javafx
+*/
+package org.martus.client.swingui.jfx;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+
+public class FxSwitchButton extends Label
+{
+	   public FxSwitchButton()
+	    {
+	        Button switchBtn = new Button();
+	        switchBtn.setOnAction(new ButtonEventHandler());
+	        setGraphic(switchBtn);
+	        switchedOn.addListener(new ButtonPressedChangeListener());
+	    }
+
+	    public SimpleBooleanProperty switchOnProperty() 
+	    { 
+	    		return switchedOn; 
+	    	}
+	    
+	    public void setSelected(boolean isSelected)
+	    {
+	    		switchedOn.set(isSelected);
+	    }
+	    
+	    public boolean isSelected()
+	    {
+	    		return switchedOn.get();
+	    }
+	    
+	
+    private final class ButtonEventHandler implements EventHandler<ActionEvent>
+	{
+		public ButtonEventHandler()
+		{
+		}
+
+		@Override
+		public void handle(ActionEvent t)
+		{
+		    switchedOn.set(!switchedOn.get());
+		}
+	}
+
+	private final class ButtonPressedChangeListener implements ChangeListener<Boolean>
+	{
+		public ButtonPressedChangeListener()
+		{
+		}
+
+		@Override
+		public void changed(ObservableValue<? extends Boolean> oldValue, Boolean currentValue, Boolean newValue)
+		{
+		    if (newValue)
+		    {
+		        setText("ON");
+		        setStyle("-fx-background-color: green;-fx-text-fill:white;");
+		        setContentDisplay(ContentDisplay.RIGHT);
+		    }
+		    else
+		    {
+		        setText("OFF");
+		        setStyle("-fx-background-color: grey;-fx-text-fill:black;");
+		        setContentDisplay(ContentDisplay.LEFT);
+		    }
+		}
+	}
+ 
+    SimpleBooleanProperty switchedOn = new SimpleBooleanProperty(true);
+}
