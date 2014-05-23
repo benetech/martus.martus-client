37c5667d08a38a9b19ee266045d26611211e6f06
Kevin Smith <Kevin Smith@localhost>
2014-05-23 13:42:29 -0700

    Rearrange member declarations for more logical grouping

--- source/org/martus/client/swingui/UiMainWindow.java
+++ source/org/martus/client/swingui/UiMainWindow.java
@@ -2977,6 +2977,15 @@ public class UiMainWindow extends JFrame implements ClipboardOwner
 	public static final String STATUS_NO_SERVER_AVAILABLE = "NoServerAvailableProgressMessage";
 	public static final String STATUS_SERVER_NOT_CONFIGURED = "ServerNotConfiguredProgressMessage";
 
+	public static final int MINIMUM_TEXT_FIELD_WIDTH = 30;
+	private static final int TESTING_TIMEOUT_60_SECONDS = 60;
+	private static final int TESTING_FOCHECK_SECONDS = 5 * 60;
+	private static final int MINIMUM_SCREEN_WIDTH = 700;
+	public static final int MAX_KEYPAIRFILE_SIZE = 32000;
+	private static final int BACKGROUND_UPLOAD_CHECK_MILLIS = 5*1000;
+	private static final int BACKGROUND_TIMEOUT_CHECK_EVERY_X_MILLIS = 5*1000;
+	private static final int TIME_BETWEEN_FIELD_OFFICE_CHECKS_SECONDS = 60 * 60;
+
 	private UiSession session;
 	
 	UiBulletinPreviewPane preview;
@@ -3003,14 +3012,6 @@ public class UiMainWindow extends JFrame implements ClipboardOwner
 	private FileOutputStream lockStream;
 	public static int timeoutInXSeconds;
 	public int timeBetweenFieldOfficeChecksSeconds;
-	public static final int MINIMUM_TEXT_FIELD_WIDTH = 30;
-	private static final int TESTING_TIMEOUT_60_SECONDS = 60;
-	private static final int TESTING_FOCHECK_SECONDS = 5 * 60;
-	private static final int MINIMUM_SCREEN_WIDTH = 700;
-	public static final int MAX_KEYPAIRFILE_SIZE = 32000;
-	private static final int BACKGROUND_UPLOAD_CHECK_MILLIS = 5*1000;
-	private static final int BACKGROUND_TIMEOUT_CHECK_EVERY_X_MILLIS = 5*1000;
-	private static final int TIME_BETWEEN_FIELD_OFFICE_CHECKS_SECONDS = 60 * 60;
 	boolean mainWindowInitalizing;
 	private boolean createdNewAccount;
 	private boolean justRecovered;
