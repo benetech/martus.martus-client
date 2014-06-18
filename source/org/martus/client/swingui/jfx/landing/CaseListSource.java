41a9c1efb81de1641ea1acefce8a4fb7506598be
charlesl <charlesl@localhost>
2014-06-18 14:18:42 -0700

    new classes to handle FX CaseLists

---
+++ source/org/martus/client/swingui/jfx/landing/CaseListSource.java
@@ -0,0 +1,73 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.landing;
+
+import java.util.ArrayList;
+
+import javafx.collections.ModifiableObservableListBase;
+
+
+public class CaseListSource extends ModifiableObservableListBase<CaseList>
+{
+	
+	public CaseListSource()
+	{
+		cases = new ArrayList<CaseList>(INITIAL_NUMBER_OF_CASES);
+	}
+
+	@Override
+	protected void doAdd(int index, CaseList caseToAdd)
+	{
+		cases.add(index, caseToAdd);
+	}
+
+	@Override
+	protected CaseList doRemove(int index)
+	{
+		return (CaseList) cases.remove(index);
+	}
+
+	@Override
+	protected CaseList doSet(int index, CaseList caseToSet)
+	{
+		return (CaseList) cases.set(index, caseToSet);
+	}
+
+	@Override
+	public CaseList get(int index)
+	{
+		return (CaseList) cases.get(index);
+	}
+
+	@Override
+	public int size()
+	{
+		return cases.size();
+	}
+	
+	final int INITIAL_NUMBER_OF_CASES = 50;
+	ArrayList cases;
+}
