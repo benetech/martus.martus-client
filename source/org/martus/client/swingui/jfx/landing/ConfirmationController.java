ece6897650257beba81f49ad7ed762fd0de1dd6d
charlesl <charlesl@localhost>
2014-07-02 06:36:43 -0700

    starting framework on how a confirmation fx dialog behaves

---
+++ source/org/martus/client/swingui/jfx/landing/ConfirmationController.java
@@ -0,0 +1,71 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.landing;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import org.martus.client.swingui.MartusLocalization;
+import org.martus.client.swingui.UiMainWindow;
+
+public class ConfirmationController extends DialogWithOkCancelContentController
+{
+
+	public ConfirmationController(UiMainWindow mainWindowToUse, String confirmationMessageTagToUse)
+	{
+		super(mainWindowToUse);
+		confirmationMessageTag = confirmationMessageTagToUse;
+	}
+
+	@Override
+	public void initialize()
+	{
+		UiMainWindow mainWindow = getMainWindow();
+		String confirmationTitle = mainWindow.getConfirmDialogTitle(confirmationMessageTag);
+		messageTitle.setText(confirmationTitle);
+		
+		String confirmationCause = mainWindow.getConfirmCauseText(confirmationMessageTag);
+		String confirmationEffect = mainWindow.getConfirmEffectText(confirmationMessageTag);
+		String totalConfirmationMessage = confirmationCause +
+				"\n" + confirmationEffect;
+		messageTextArea.setText(totalConfirmationMessage);
+	}
+
+	@Override
+	public String getFxmlLocation()
+	{
+		return "landing/Confirmation.fxml";
+	}
+	
+	@FXML 
+	Label messageTitle;
+	
+	@FXML 
+	TextArea messageTextArea;
+
+	private String confirmationMessageTag;
+	}
