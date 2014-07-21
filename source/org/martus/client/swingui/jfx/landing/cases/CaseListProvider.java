eb14dd16f628d5341d8ab1d79ff9265e739fd8de
Kevin Smith <Kevin Smith@localhost>
2014-07-21 15:57:59 -0700

    Refactor move some generic classes into data and controls subpackages

--- source/org/martus/client/swingui/jfx/landing/cases/CaseListProvider.java
+++ source/org/martus/client/swingui/jfx/landing/cases/CaseListProvider.java
@@ -25,7 +25,7 @@ Boston, MA 02111-1307, USA.
 */
 package org.martus.client.swingui.jfx.landing.cases;
 
-import org.martus.client.swingui.jfx.generic.ArrayObservableList;
+import org.martus.client.swingui.jfx.generic.data.ArrayObservableList;
 
 
 
