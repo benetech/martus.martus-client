c0dae0da778161c83d81617460f4eaa87526428c
Kevin Smith <Kevin Smith@localhost>
2014-08-26 11:07:47 -0700

    Refactor create interface from class (and use it where possible)

---
+++ source/org/martus/client/swingui/bulletincomponent/UiBulletinComponentInterface.java
@@ -0,0 +1,74 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+ */
+
+package org.martus.client.swingui.bulletincomponent;
+
+import java.io.IOException;
+
+import javax.swing.event.ChangeEvent;
+
+import org.martus.client.core.BulletinLanguageChangeListener;
+import org.martus.client.core.EncryptionChangeListener;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.common.bulletin.Bulletin;
+import org.martus.common.crypto.MartusCrypto;
+import org.martus.common.fieldspec.DataInvalidException;
+
+public interface UiBulletinComponentInterface
+{
+
+	abstract public void setEncryptionChangeListener(
+			EncryptionChangeListener listener);
+
+	abstract public void setLanguageChangeListener(
+			BulletinLanguageChangeListener listener);
+
+	abstract public void copyDataToBulletin(Bulletin bulletin)
+			throws IOException, MartusCrypto.EncryptionException;
+
+	abstract public void validateData() throws DataInvalidException;
+
+	abstract public boolean isBulletinModified() throws Exception;
+
+	// ChangeListener interface
+	abstract public void stateChanged(ChangeEvent event);
+
+	// LanguageChangeListener interface
+	public abstract void bulletinLanguageHasChanged(
+			String newBulletinLanguageCode);
+
+	public abstract void copyDataFromBulletin(Bulletin bulletinToShow)
+			throws Exception;
+
+	public abstract void updateEncryptedIndicator(boolean isEncrypted);
+
+	public abstract void encryptAndDisableAllPrivate();
+
+	public abstract boolean isAllPrivateBoxChecked();
+
+	public abstract UiMainWindow getMainWindow();
+
+}
