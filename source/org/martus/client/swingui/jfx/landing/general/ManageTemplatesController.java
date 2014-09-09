7adb2d2ff9c450a3cc1e49fb52314e3fe1745d08
Kevin Smith <Kevin Smith@localhost>
2014-09-09 12:32:25 -0700

    Switch to tab explicitly instead of by a potentially-fragile index

--- source/org/martus/client/swingui/jfx/landing/general/ManageTemplatesController.java
+++ source/org/martus/client/swingui/jfx/landing/general/ManageTemplatesController.java
@@ -43,6 +43,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -420,7 +421,7 @@ public class ManageTemplatesController extends FxInSwingController
 			populateAvailableTemplatesTable();
 			templateToAddProperty.setValue(null);
 			
-			tabPane.selectionModelProperty().get().clearAndSelect(0);
+			tabPane.selectionModelProperty().get().select(availableTemplatesTab);
 			availableTemplatesTable.selectionModelProperty().get().clearSelection();
 			availableTemplatesTable.scrollTo(0);
 		}
@@ -437,6 +438,9 @@ public class ManageTemplatesController extends FxInSwingController
 	
 	@FXML
 	private TabPane tabPane;
+	
+	@FXML
+	private Tab availableTemplatesTab;
 	
 	@FXML
 	private TableView<ManageTemplatesTableRowData> availableTemplatesTable;
