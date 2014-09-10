454475a81e8a3b2f6b3dbc90290360b180b3962c
Kevin Smith <Kevin Smith@localhost>
2014-09-10 14:11:52 -0700

    Editor now shows a list of field labels for the bulletin

--- source/org/martus/client/swingui/jfx/landing/bulletins/BulletinEditorBodyController.java
+++ source/org/martus/client/swingui/jfx/landing/bulletins/BulletinEditorBodyController.java
@@ -25,13 +25,17 @@ Boston, MA 02111-1307, USA.
 */
 package org.martus.client.swingui.jfx.landing.bulletins;
 
+import java.util.Vector;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 import org.martus.client.core.FxBulletin;
 import org.martus.client.swingui.UiMainWindow;
 import org.martus.client.swingui.jfx.generic.FxController;
+import org.martus.common.fieldspec.FieldSpec;
+import org.martus.common.fieldspec.StandardFieldSpecs;
 
 public class BulletinEditorBodyController extends FxController
 {
@@ -48,10 +52,20 @@ public class BulletinEditorBodyController extends FxController
 
 	public void showBulletin(FxBulletin bulletinToShow) throws Exception
 	{
-		Label label = new Label("(Bulletin contents would go here)");
-		bodyPane.getChildren().add(label);
+		Vector<FieldSpec> fieldTags = bulletinToShow.getFieldSpecs();
+		fieldTags.forEach(spec -> createFieldForSpec(spec));
+	}
+
+	private void createFieldForSpec(FieldSpec spec)
+	{
+		String tag = spec.getTag();
+		String labelText = spec.getLabel();
+		if(StandardFieldSpecs.isStandardFieldTag(tag))
+			labelText = getLocalization().getFieldLabel(tag);
+		Label label = new Label(labelText + ":");
+		fieldsBox.getChildren().add(label);
 	}
 
 	@FXML
-	private Pane bodyPane;
+	private VBox fieldsBox;
 }
