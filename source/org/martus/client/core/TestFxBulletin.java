6d828bb36ae43facbf09f947e2151878e5d50820
charlesl <charlesl@localhost>
2014-09-12 10:47:29 -0700

    New authorizedToReadProperty along with clearing of old VersionProperty which sets the value to 0.

--- source/org/martus/client/core/TestFxBulletin.java
+++ source/org/martus/client/core/TestFxBulletin.java
@@ -32,6 +32,8 @@ import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleStringProperty;
 
 import org.martus.common.FieldSpecCollection;
+import org.martus.common.HeadquartersKey;
+import org.martus.common.HeadquartersKeys;
 import org.martus.common.bulletin.Bulletin;
 import org.martus.common.bulletin.BulletinForTesting;
 import org.martus.common.crypto.MockMartusSecurity;
@@ -75,6 +77,7 @@ public class TestFxBulletin extends TestCaseEnhanced
 		bulletinWith3Versions.setHistory(localHistory);
 		assertEquals("Bulletin2 doesn't have 3 versions?", 3, bulletinWith3Versions.getVersion());
 		fxb.copyDataFromBulletin(bulletinWith3Versions);
+		assertEquals(Integer.valueOf(0), versionProperty.getValue());
 		versionProperty = fxb.getVersionProperty();
 		assertEquals(Integer.valueOf(bulletinWith3Versions.getVersion()), versionProperty.getValue());
 		assertEquals(Integer.valueOf(3), versionProperty.getValue());
@@ -99,6 +102,38 @@ public class TestFxBulletin extends TestCaseEnhanced
 		assertEquals(b2.getUniversalId(), universalIdProperty2.getValue());
 	}
 	
+	public void testAuthorizedToRead() throws Exception
+	{
+		FxBulletin fxb = new FxBulletin();
+		ReadOnlyObjectWrapper<HeadquartersKeys> headquartersKeysPropertyNull = fxb.authorizedToReadProperty();
+		assertEquals(null, headquartersKeysPropertyNull);
+		
+		Bulletin b = new BulletinForTesting(security);
+		HeadquartersKey key1 = new HeadquartersKey("account1");
+		Vector keysToUse = new Vector();
+		keysToUse.add(key1);
+		HeadquartersKeys keys = new HeadquartersKeys(keysToUse);
+		b.setAuthorizedToReadKeys(keys);
+		
+		fxb.copyDataFromBulletin(b);
+		ReadOnlyObjectWrapper<HeadquartersKeys> headquartersKeysProperty = fxb.authorizedToReadProperty();
+		assertEquals(b.getAuthorizedToReadKeys(), headquartersKeysProperty.getValue());
+		
+		Bulletin b2 = new BulletinForTesting(security);
+		HeadquartersKey key2 = new HeadquartersKey("account2");
+		HeadquartersKey key3 = new HeadquartersKey("account3");
+		Vector newKeys = new Vector();
+		newKeys.add(key2);
+		newKeys.add(key3);
+		HeadquartersKeys b2keys = new HeadquartersKeys(keysToUse);
+		b2.setAuthorizedToReadKeys(b2keys);
+
+		fxb.copyDataFromBulletin(b2);
+		assertEquals(null, headquartersKeysProperty.getValue());
+		ReadOnlyObjectWrapper<HeadquartersKeys> authorizedToReadProperty2 = fxb.authorizedToReadProperty();
+		assertEquals(b2.getAuthorizedToReadKeys(), authorizedToReadProperty2.getValue());
+	}
+
 	public void testTitle() throws Exception
 	{
 		FxBulletin fxb = new FxBulletin();
