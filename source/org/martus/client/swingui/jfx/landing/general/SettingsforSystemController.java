e07803bd0588ced654a98a6a1aab37db23848a3e
charlesl <charlesl@localhost>
2014-09-26 11:25:20 -0700

    renamed NeverDeleteSnapshotFromServer to immutableOnServer for UI Settings

--- source/org/martus/client/swingui/jfx/landing/general/SettingsforSystemController.java
+++ source/org/martus/client/swingui/jfx/landing/general/SettingsforSystemController.java
@@ -64,7 +64,7 @@ public class SettingsforSystemController extends FxInSwingController
 		super.initialize(location, bundle);
 		ConfigInfo configInfo = getApp().getConfigInfo();
 		useZawgyiFont.selectedProperty().setValue(configInfo.getUseZawgyiFont());
-		neverDeleteSnapshotFromServer.selectedProperty().setValue(configInfo.getNeverDeleteSnapshotFromServer());
+		immutableOnServer.selectedProperty().setValue(configInfo.getNeverDeleteSnapshotFromServer());
 		MartusLocalization localization = getLocalization();
 		initializeLanguageChoices(localization);
 		initializeDateFormatChoices(localization);
@@ -156,7 +156,7 @@ public class SettingsforSystemController extends FxInSwingController
 		MartusLocalization localization = getLocalization();
 		ConfigInfo configInfo = getApp().getConfigInfo();
 		configInfo.setUseZawgyiFont(useZawgyiFont.selectedProperty().getValue());
-		configInfo.setNeverDeleteSnapshotFromServer(neverDeleteSnapshotFromServer.selectedProperty().getValue());
+		configInfo.setNeverDeleteSnapshotFromServer(immutableOnServer.selectedProperty().getValue());
 		localization.setMdyOrder(dateFormat.getSelectionModel().getSelectedItem().getCode());
 		String delimiter = dateDelimiter.getSelectionModel().getSelectedItem().getCode();
 		localization.setDateDelimiter(delimiter.charAt(0));
@@ -211,6 +211,6 @@ public class SettingsforSystemController extends FxInSwingController
 	private ChoiceBox<ChoiceItem> calendarType;
 
 	@FXML
-	private CheckBox neverDeleteSnapshotFromServer;
+	private CheckBox immutableOnServer;
 	
 }
