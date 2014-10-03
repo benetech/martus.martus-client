42f63b6c0328113270af4be9b4aab8a16c9e442b
Kevin Smith <Kevin Smith@localhost>
2014-10-03 15:42:58 -0700

    Refactor move class

--- source/org/martus/client/swingui/jfx/generic/controls/NestedChoiceBox.java
+++ source/org/martus/client/swingui/jfx/generic/controls/NestedChoiceBox.java
@@ -23,7 +23,7 @@ Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.
 
 */
-package org.martus.client.swingui.jfx.landing.bulletins;
+package org.martus.client.swingui.jfx.generic.controls;
 
 import java.util.Vector;
 import java.util.function.Predicate;
