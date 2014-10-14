83ea772e18c48c3a1213e20e2c3c8603dab4122d
Kevin Smith <Kevin Smith@localhost>
2014-10-14 16:18:37 -0700

    Just rearrange the code within the class. Nothing more.

--- source/org/martus/client/core/FxBulletinGridField.java
+++ source/org/martus/client/core/FxBulletinGridField.java
@@ -45,7 +45,162 @@ import org.martus.common.fieldspec.GridFieldSpec;
 
 public class FxBulletinGridField extends FxBulletinField
 {
-	static class ColumnValues extends ObservableChoiceItemList
+	public GridFieldSpec getGridFieldSpec()
+	{
+		return (GridFieldSpec) getFieldSpec();
+	}
+
+	public GridFieldData gridDataProperty()
+	{
+		if(!isGrid())
+			throw new RuntimeException("gridDataProperty not available for non-grid: " + getTag());
+	
+		return gridDataIfApplicable;
+	}
+
+	public String getValue()
+	{
+		updateOverallValue();
+		
+		return valueProperty().getValue();
+	}
+
+	public void setValue(String value)
+	{
+		setGridData(value);
+		
+		valueProperty().setValue(value);
+	}
+
+	public GridRowFields appendEmptyGridRow()
+	{
+		if(!isGrid())
+			throw new RuntimeException("Cannot append rows to non-grid field: " + getTag());
+		
+		GridRowFields newRowFields = createEmptyRow();
+		gridDataProperty().add(newRowFields);
+		return newRowFields;
+	}
+
+	public void removeGridRow(GridRowFields rowToRemove)
+	{
+		GridFieldData gridData = gridDataProperty();
+		if(!gridData.contains(rowToRemove))
+			throw new RuntimeException("Attempted to remove grid row that didn't exist");
+		
+		gridData.remove(rowToRemove);
+	}
+
+	public Vector<ObservableChoiceItemList> gridColumnValuesProperty(String columnLabel)
+	{
+		if(!isGrid())
+			throw new RuntimeException("gridColumnValues not available for non-grid: " + getTag());
+		
+		GridFieldSpec gridSpec = getGridFieldSpec();
+		FieldSpec columnSpec = gridSpec.findColumnSpecByLabel(columnLabel);
+		if(columnSpec == null)
+			throw new RuntimeException("attempt to get values from non-existent grid column: " + getTag() + " . " + columnLabel);
+	
+		FxBulletinGridField.ColumnValues columnValues = gridColumnValuesMap.get(columnLabel);
+		if(columnValues == null)
+		{
+			columnValues = new FxBulletinGridField.ColumnValues(this, columnLabel, getLocalization());
+			gridColumnValuesMap.put(columnLabel, columnValues);
+		}
+		
+		Vector<ObservableChoiceItemList> listOfLists = new Vector<ObservableChoiceItemList>();
+		listOfLists.add(columnValues);
+		return listOfLists;
+	}
+
+	protected FxBulletinGridField(FxBulletin bulletinToUse, GridFieldSpec fieldSpecToUse, MiniLocalization localizationToUse)
+	{
+		super(bulletinToUse, fieldSpecToUse, localizationToUse);
+
+		gridDataIfApplicable = new GridFieldData();
+		ListChangeListener<GridRowFields> rowChangeHandler = (change) -> updateOverallValue();
+		gridDataIfApplicable.addListener(rowChangeHandler);
+		gridColumnValuesMap = new HashMap<String, FxBulletinGridField.ColumnValues>();
+	}
+
+	protected void updateOverallValue()
+	{
+		String newValue = getGridValue();
+		valueProperty().setValue(newValue);
+	}
+
+	private void setGridData(String xmlGridData)
+	{
+		gridDataProperty().clear();
+		
+		GridFieldSpec gridSpec = getGridFieldSpec();
+		PoolOfReusableChoicesLists poolOfReusableChoicesLists = getBulletin().getAllReusableChoicesLists();
+		GridData data = new GridData(gridSpec, poolOfReusableChoicesLists );
+		
+		try
+		{
+			data.setFromXml(xmlGridData);
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+		
+		for(int row = 0; row < data.getRowCount(); ++row)
+		{
+			GridRow gridRow = data.getRow(row);
+			GridRowFields rowFields = appendEmptyGridRow();
+			copyGridRowToGridRowFields(gridRow, rowFields);
+		}
+	}
+
+	private String getGridValue()
+	{
+		PoolOfReusableChoicesLists irrelevantReusableLists = null;
+		GridFieldSpec gridSpec = getGridFieldSpec();
+		GridData gridData = new GridData(gridSpec, irrelevantReusableLists);
+		for(int row = 0; row < gridDataProperty().size(); ++ row)
+		{
+			GridRowFields rowFields = gridDataProperty().get(row);
+			GridRow gridRow = convertGridRowFieldsToGridRow(gridSpec, rowFields);
+			if(!gridRow.isEmptyRow())
+				gridData.addRow(gridRow);
+		}
+	
+		return gridData.getXmlRepresentation();
+	}
+
+	private GridRowFields createEmptyRow()
+	{
+		GridFieldSpec gridFieldSpec = getGridFieldSpec();
+		GridRowFields rowFields = new GridRowFields();
+		for(int column = 0; column < gridFieldSpec.getColumnCount(); ++column)
+		{
+			String columnLabel = gridFieldSpec.getColumnLabel(column);
+			FieldSpec cellSpec = gridFieldSpec.getFieldSpec(column);
+			FxBulletinField cellField = createFxBulletinField(getBulletin(), cellSpec, getLocalization());
+			rowFields.put(columnLabel, cellField);
+			
+			cellField.addValueListener((observable, oldValue, newValue) -> updateOverallValue());
+		}
+	
+		GridRow gridRow = GridRow.createEmptyRow(getGridFieldSpec(), PoolOfReusableChoicesLists.EMPTY_POOL);
+		copyGridRowToGridRowFields(gridRow, rowFields);
+		return rowFields;
+	}
+
+	private void copyGridRowToGridRowFields(GridRow gridRow, GridRowFields rowFields)
+	{
+		for(int column = 0; column < gridRow.getColumnCount(); ++column)
+		{
+			String columnLabel = getGridFieldSpec().getColumnLabel(column);
+			FxBulletinField cellField = rowFields.get(columnLabel);
+			String value = gridRow.getCellText(column);
+			cellField.setValue(value);
+		}
+	}
+
+	private static class ColumnValues extends ObservableChoiceItemList
 	{
 		public ColumnValues(FxBulletinGridField gridField, String gridColumnLabel, MiniLocalization localizationToUse)
 		{
@@ -147,160 +302,4 @@ public class FxBulletinGridField extends FxBulletinField
 
 	private GridFieldData gridDataIfApplicable;
 	private Map<String, FxBulletinGridField.ColumnValues> gridColumnValuesMap;
-
-	protected FxBulletinGridField(FxBulletin bulletinToUse, GridFieldSpec fieldSpecToUse, MiniLocalization localizationToUse)
-	{
-		super(bulletinToUse, fieldSpecToUse, localizationToUse);
-
-		gridDataIfApplicable = new GridFieldData();
-		ListChangeListener<GridRowFields> rowChangeHandler = (change) -> updateOverallValue();
-		gridDataIfApplicable.addListener(rowChangeHandler);
-		gridColumnValuesMap = new HashMap<String, FxBulletinGridField.ColumnValues>();
-	}
-
-	public GridFieldSpec getGridFieldSpec()
-	{
-		return (GridFieldSpec) getFieldSpec();
-	}
-
-	public GridFieldData gridDataProperty()
-	{
-		if(!isGrid())
-			throw new RuntimeException("gridDataProperty not available for non-grid: " + getTag());
-	
-		return gridDataIfApplicable;
-	}
-
-	public String getValue()
-	{
-		updateOverallValue();
-		
-		return valueProperty().getValue();
-	}
-
-	public void setValue(String value)
-	{
-		setGridData(value);
-		
-		valueProperty().setValue(value);
-	}
-
-	private void setGridData(String xmlGridData)
-	{
-		gridDataProperty().clear();
-		
-		GridFieldSpec gridSpec = getGridFieldSpec();
-		PoolOfReusableChoicesLists poolOfReusableChoicesLists = getBulletin().getAllReusableChoicesLists();
-		GridData data = new GridData(gridSpec, poolOfReusableChoicesLists );
-		
-		try
-		{
-			data.setFromXml(xmlGridData);
-		}
-		catch(Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-		
-		for(int row = 0; row < data.getRowCount(); ++row)
-		{
-			GridRow gridRow = data.getRow(row);
-			GridRowFields rowFields = appendEmptyGridRow();
-			copyGridRowToGridRowFields(gridRow, rowFields);
-		}
-	}
-
-	public GridRowFields appendEmptyGridRow()
-	{
-		if(!isGrid())
-			throw new RuntimeException("Cannot append rows to non-grid field: " + getTag());
-		
-		GridRowFields newRowFields = createEmptyRow();
-		gridDataProperty().add(newRowFields);
-		return newRowFields;
-	}
-
-	public void removeGridRow(GridRowFields rowToRemove)
-	{
-		GridFieldData gridData = gridDataProperty();
-		if(!gridData.contains(rowToRemove))
-			throw new RuntimeException("Attempted to remove grid row that didn't exist");
-		
-		gridData.remove(rowToRemove);
-	}
-
-	public Vector<ObservableChoiceItemList> gridColumnValuesProperty(String columnLabel)
-	{
-		if(!isGrid())
-			throw new RuntimeException("gridColumnValues not available for non-grid: " + getTag());
-		
-		GridFieldSpec gridSpec = getGridFieldSpec();
-		FieldSpec columnSpec = gridSpec.findColumnSpecByLabel(columnLabel);
-		if(columnSpec == null)
-			throw new RuntimeException("attempt to get values from non-existent grid column: " + getTag() + " . " + columnLabel);
-	
-		FxBulletinGridField.ColumnValues columnValues = gridColumnValuesMap.get(columnLabel);
-		if(columnValues == null)
-		{
-			columnValues = new FxBulletinGridField.ColumnValues(this, columnLabel, getLocalization());
-			gridColumnValuesMap.put(columnLabel, columnValues);
-		}
-		
-		Vector<ObservableChoiceItemList> listOfLists = new Vector<ObservableChoiceItemList>();
-		listOfLists.add(columnValues);
-		return listOfLists;
-	}
-
-	private String getGridValue()
-	{
-		PoolOfReusableChoicesLists irrelevantReusableLists = null;
-		GridFieldSpec gridSpec = getGridFieldSpec();
-		GridData gridData = new GridData(gridSpec, irrelevantReusableLists);
-		for(int row = 0; row < gridDataProperty().size(); ++ row)
-		{
-			GridRowFields rowFields = gridDataProperty().get(row);
-			GridRow gridRow = convertGridRowFieldsToGridRow(gridSpec, rowFields);
-			if(!gridRow.isEmptyRow())
-				gridData.addRow(gridRow);
-		}
-	
-		return gridData.getXmlRepresentation();
-	}
-
-	private GridRowFields createEmptyRow()
-	{
-		GridFieldSpec gridFieldSpec = getGridFieldSpec();
-		GridRowFields rowFields = new GridRowFields();
-		for(int column = 0; column < gridFieldSpec.getColumnCount(); ++column)
-		{
-			String columnLabel = gridFieldSpec.getColumnLabel(column);
-			FieldSpec cellSpec = gridFieldSpec.getFieldSpec(column);
-			FxBulletinField cellField = createFxBulletinField(getBulletin(), cellSpec, getLocalization());
-			rowFields.put(columnLabel, cellField);
-			
-			cellField.addValueListener((observable, oldValue, newValue) -> updateOverallValue());
-		}
-	
-		GridRow gridRow = GridRow.createEmptyRow(getGridFieldSpec(), PoolOfReusableChoicesLists.EMPTY_POOL);
-		copyGridRowToGridRowFields(gridRow, rowFields);
-		return rowFields;
-	}
-
-	private void copyGridRowToGridRowFields(GridRow gridRow, GridRowFields rowFields)
-	{
-		for(int column = 0; column < gridRow.getColumnCount(); ++column)
-		{
-			String columnLabel = getGridFieldSpec().getColumnLabel(column);
-			FxBulletinField cellField = rowFields.get(columnLabel);
-			String value = gridRow.getCellText(column);
-			cellField.setValue(value);
-		}
-	}
-
-	protected void updateOverallValue()
-	{
-		String newValue = getGridValue();
-		valueProperty().setValue(newValue);
-	}
-
 }
