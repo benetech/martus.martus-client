6052cb7fdca3eea2e81622a2c8c5d9ee89c50323
charlesl <charlesl@localhost>
2014-10-16 06:21:12 -0700

    Pushed down methods to subclass, manually added @Override and new abstract method signatures to overridden methods.  Using new subclass in editor.

--- source/org/martus/client/swingui/jfx/landing/bulletins/FxFieldCreator.java
+++ source/org/martus/client/swingui/jfx/landing/bulletins/FxFieldCreator.java
@@ -25,33 +25,15 @@ Boston, MA 02111-1307, USA.
 */
 package org.martus.client.swingui.jfx.landing.bulletins;
 
-import java.util.Vector;
-
-import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.Property;
 import javafx.scene.Node;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 
 import org.martus.client.core.FxBulletin;
 import org.martus.client.core.FxBulletinField;
 import org.martus.client.swingui.MartusLocalization;
-import org.martus.client.swingui.jfx.generic.controls.DateRangePicker;
-import org.martus.client.swingui.jfx.generic.controls.MartusDatePicker;
-import org.martus.client.swingui.jfx.generic.controls.NestedChoiceBox;
-import org.martus.client.swingui.jfx.generic.controls.ScrollFreeTextArea;
-import org.martus.client.swingui.jfx.generic.data.BooleanStringConverter;
-import org.martus.client.swingui.jfx.generic.data.ObservableChoiceItemList;
-import org.martus.common.bulletin.Bulletin;
 import org.martus.common.fieldspec.FieldSpec;
-import org.martus.common.fieldspec.MessageFieldSpec;
 
-public class FxFieldCreator
+abstract public class FxFieldCreator
 {
 	public FxFieldCreator(MartusLocalization localizationToUse)
 	{
@@ -87,95 +69,14 @@ public class FxFieldCreator
 		return createFieldNotAvailable();
 	}
 
-	protected Node createDateField(FxBulletinField field)
-	{
-		MartusDatePicker picker = new MartusDatePicker(localization);
-
-		if(field.getTag().equals(Bulletin.TAGENTRYDATE))
-			picker.setDisable(true);
-
-		Property<String> property = field.valueProperty();
-		String existingDateString = property.getValue();
-		picker.setValue(existingDateString);
-		property.bind(picker.overallValueProperty());
-		return picker;
-	}
-	
-	protected Node createDateRangeField(FxBulletinField field)
-	{
-		DateRangePicker picker = new DateRangePicker(localization);
-
-		Property<String> property = field.valueProperty();
-		String existingDateRangeString = property.getValue();
-		picker.setValue(existingDateRangeString);
-		property.bind(picker.valueProperty());
-		
-		return picker;
-	}
-	
-	protected Node createDropdownField(FxBulletin bulletin, FxBulletinField field) throws Exception
-	{
-		Vector<ObservableChoiceItemList> listOfChoiceItemLists = field.getChoiceItemLists();
-		if(listOfChoiceItemLists.size() == 0)
-			return createFieldNotAvailable();
-
-		NestedChoiceBox choiceBoxes = new NestedChoiceBox();
-		choiceBoxes.setChoiceItemLists(listOfChoiceItemLists);
-
-		Property<String> property = field.valueProperty();
-		choiceBoxes.setValue(property.getValue());
-		property.bind(choiceBoxes.valueProperty());
-
-		return choiceBoxes;
-	}
-
-	protected Node createBooleanField(Property<String> property)
-	{
-		CheckBox checkBox = new CheckBox();
-		BooleanStringConverter converter = new BooleanStringConverter();
-		checkBox.selectedProperty().setValue(converter.fromString(property.getValue()));
-
-		BooleanProperty selectedStateProperty = checkBox.selectedProperty();
-		selectedStateProperty.addListener(
-			(observable, oldValue, newValue) -> property.setValue(converter.toString(newValue))
-		);
-		return checkBox;
-	}
-
-	protected Node createMessageField(FieldSpec spec)
-	{
-		String messageText = ((MessageFieldSpec)(spec)).getMessage();
-		Text text = new Text(messageText);
-		TextFlow flow = new TextFlow(text);
-		flow.getStyleClass().add("messageText");
-		return flow;
-	}
-
-	protected Node createStringField(Property<String> property)
-	{
-		ScrollFreeTextArea textField = new ScrollFreeTextArea();
-		textField.textProperty().bindBidirectional(property);
-		HBox.setHgrow(textField, Priority.SOMETIMES);
-		
-		return textField;
-	}
-	
-	protected Node createMultilineField(Property<String> property)
-	{
-		TextArea textArea = new TextArea();
-		textArea.setPrefColumnCount(MINIMUM_REASONABLE_COLUMN_COUNT);
-		textArea.setPrefRowCount(MULTILINE_FIELD_HEIGHT_IN_ROWS);
-		textArea.setWrapText(true);
-		textArea.textProperty().bindBidirectional(property);
-		HBox.setHgrow(textArea, Priority.SOMETIMES);
-		
-		return textArea;
-	}
-	
-	protected Node createFieldNotAvailable()
-	{
-		return new Label("(n/a)");
-	}
+	abstract protected Node createStringField(Property<String> property);
+	abstract protected Node createMultilineField(Property<String> property);
+	abstract protected Node createMessageField(FieldSpec spec);
+	abstract protected Node createBooleanField(Property<String> property);
+	abstract protected Node createDropdownField(FxBulletin bulletin, FxBulletinField field) throws Exception;
+	abstract protected Node createDateField(FxBulletinField field);
+	abstract protected Node createDateRangeField(FxBulletinField field);
+	abstract protected Node createFieldNotAvailable();
 
 	private static final int MINIMUM_REASONABLE_COLUMN_COUNT = 10;
 	private static final int MULTILINE_FIELD_HEIGHT_IN_ROWS = 5;
