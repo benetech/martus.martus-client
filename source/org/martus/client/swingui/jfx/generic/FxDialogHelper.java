8537c12ff0ab017a6e6a0f9515ec9d54a70b6682
nimaa <nimaa@localhost>
2014-10-31 13:19:59 -0400

    martusdev-819: added new confirmation dialog which can be called from swing thread.   Close bulletin page now calls this new fx confirmation dialog instead of swing.

---
+++ source/org/martus/client/swingui/jfx/generic/FxDialogHelper.java
@@ -0,0 +1,75 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.generic;
+
+import java.util.Map;
+
+import org.martus.client.swingui.UiMainWindow;
+
+public class FxDialogHelper
+{
+	public static void showNotificationDialog(UiMainWindow mainWindow, String baseTag)
+	{
+		showNotificationDialog(mainWindow, baseTag, null);
+	}
+	
+	public static void showNotificationDialog(UiMainWindow mainWindow, String baseTag, Map map)
+	{
+		try
+		{
+			String causeTag = "notify" + baseTag + "cause";
+			FxController mainNotificationAreaController = new SimpleTextContentController(mainWindow, causeTag, map);
+			DialogWithCloseShellController dialogWithCloseShellController = new DialogWithCloseShellController(mainWindow, mainNotificationAreaController);
+			DialogStage stage = new DialogStage(mainWindow, dialogWithCloseShellController);
+			FxModalDialog.createAndShow(mainWindow, stage);
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static boolean showConfirmationDialog(UiMainWindow mainWindow, String baseTag)
+	{
+		try
+		{
+			String causeTag = "confirm" + baseTag + "effect";
+			FxController mainNotificationAreaController = new SimpleTextContentController(mainWindow, causeTag);
+			DialogWithYesNoShellController dialogWithCloseShellController = new DialogWithYesNoShellController(mainWindow, mainNotificationAreaController);
+			DialogStage stage = new DialogStage(mainWindow, dialogWithCloseShellController);
+			FxModalDialog.createAndShow(mainWindow, stage);
+			
+			return dialogWithCloseShellController.didConfirm();
+		} 
+		catch (Exception e)
+		{
+			
+			return false;
+		}
+	}
+	
+	
+}
