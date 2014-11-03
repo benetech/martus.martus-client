8743aa0f3b42746c7309665b0f4db5085d724d2e
charlesl <charlesl@localhost>
2014-11-03 09:55:37 -0800

    fixed NPE where a default case was being viewed but returning always null

--- source/org/martus/client/swingui/jfx/landing/cases/FxCaseManagementController.java
+++ source/org/martus/client/swingui/jfx/landing/cases/FxCaseManagementController.java
@@ -193,7 +193,11 @@ public class FxCaseManagementController extends AbstractFxLandingContentControll
 	
 	public void folderContentsHaveChanged()
 	{
-		updateCases(getCurrentBulletinFolder().getName());
+		BulletinFolder currentBulletinFolder = getCurrentBulletinFolder();
+		if(currentBulletinFolder == null)
+			updateCasesSelectDefaultCase();
+		else
+			updateCases(currentBulletinFolder.getName());
 	}
 	
 
@@ -218,7 +222,7 @@ public class FxCaseManagementController extends AbstractFxLandingContentControll
 	{
 		int selectedIndex = currentCasesListView.getSelectionModel().getSelectedIndex();
 		if(selectedIndex == INVALID_INDEX)
-			return null;
+			return defaultCaseBeingViewed;
 		CaseListItem selectedCase = getCurrentCaseListProvider().get(selectedIndex);
 		BulletinFolder folder = getApp().findFolder(selectedCase.getName());
 		
@@ -493,9 +497,10 @@ public class FxCaseManagementController extends AbstractFxLandingContentControll
 		getFxLandingShellController().setTitleBarToRecieved();
 	}
 
-	public void showDefaultCase(BulletinFolder receivedFolder)
+	public void showDefaultCase(BulletinFolder folder)
 	{
-		listeners.forEach(listener -> listener.folderWasSelected(receivedFolder));
+		defaultCaseBeingViewed = folder;
+		listeners.forEach(listener -> listener.folderWasSelected(folder));
 		clearCases();
 	}	
 	
@@ -599,4 +604,5 @@ public class FxCaseManagementController extends AbstractFxLandingContentControll
 	
 	private ListView<CaseListItem> currentCasesListView;
 	private Set<FolderSelectionListener> listeners;
+	private BulletinFolder defaultCaseBeingViewed;
 }
