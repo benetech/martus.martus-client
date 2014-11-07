bbb3eb0f0c936161536eda53d1988b8b2f7dd475
charlesl <charlesl@localhost>
2014-11-07 15:47:27 -0800

    MARTUSDEV-841 Security Issues, new wording and layout of this verification dialog

---
+++ source/org/martus/client/swingui/jfx/contacts/ContactsStage.java
@@ -0,0 +1,87 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.contacts;
+
+import javax.swing.JOptionPane;
+
+import org.martus.client.swingui.MartusLocalization;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.jfx.generic.FxController;
+import org.martus.client.swingui.jfx.generic.FxNonWizardStage;
+
+public class ContactsStage extends FxNonWizardStage
+{
+	public ContactsStage(UiMainWindow mainWindow) throws Exception
+	{
+		super(mainWindow);
+		
+		shellController = new ContactsShellController(getMainWindow());
+		contentController = new FxManageContactsController(getMainWindow());
+
+		setShellController(shellController);
+	}
+
+	@Override
+	public void showCurrentPage() throws Exception
+	{
+		// NOTE: Special case for Contacts because it is still treated 
+		// as a wizard page
+		loadAndShowCurrentWizardPage(contentController);
+	}
+	
+	public void loadAndShowCurrentWizardPage(FxController contentPaneController) throws Exception
+	{
+		loadAndShowShell();
+		
+		contentPaneController.setShellController(getShellController());
+		shellController.loadAndIntegrateContentPane(contentPaneController);
+	}
+	
+	@Override
+	protected String getCssName()
+	{
+		return "Contacts.css";
+	}
+	
+	@Override
+	protected boolean confirmExit()
+	{
+		if (!contentController.hasContactsDataChanged())
+			return true;
+		
+		MartusLocalization localization = getMainWindow().getLocalization();
+		String title = localization.getWindowTitle("ExitManageContacts");
+		String message = localization.getFieldLabel("ExitManageContacts");
+		int result = JOptionPane.showConfirmDialog(getDialog(), message, title, JOptionPane.YES_NO_OPTION);
+		if (result == JOptionPane.YES_OPTION)
+			return true;
+		
+		return false;
+	}
+	
+	private ContactsShellController shellController;
+	private FxManageContactsController contentController;
+}
