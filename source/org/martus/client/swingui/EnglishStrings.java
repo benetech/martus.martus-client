94f7942bf7be83753dc84323780d3308e5d48565
nimaa <nimaa@localhost>
2014-11-10 13:35:01 -0500

    Text Change (Spreadsheet by Erin)

--- source/org/martus/client/swingui/EnglishStrings.java
+++ source/org/martus/client/swingui/EnglishStrings.java
@@ -1935,7 +1935,7 @@ public class EnglishStrings
 
 "field:KeypairTooLarge=keypair file too large!",
 "field:BackupYourKey=Backup Your Key",
-"field:BackupYourKeyInformation=Your encryption key was created when you created your account. You will want to back it up, to be able restore your account.",
+"field:BackupYourKeyInformation=Back it your encryption key to be able to restore your account.",
 "button:CreateSingleEncryptedFile=Export Password Protected Key",
 "field:CreateSingleEncryptedFileInformation=Note: this backup method still requires remembering your username and password to restore your account.",
 "field:BackupYourKeyTip1=Be sure to save a copy of your account key to a secure location that is different from the computer where you currently have Martus installed, so you have it if you need to restore your account.",
