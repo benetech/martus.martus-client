4a536970b65997de55b583327ddffe8f2ce2cd05
nimaa <nimaa@localhost>
2014-11-10 16:05:22 -0500

    Pulled text out of fxml and moved into EnglishStrings (Spreadsheet by Erin)

--- source/org/martus/client/swingui/EnglishStrings.java
+++ source/org/martus/client/swingui/EnglishStrings.java
@@ -401,6 +401,7 @@ public class EnglishStrings
 
 "field:notifyLegacyPublicIsPrivateMessagecause=Records created in older versions of Martus may contain fields of data that may have been made public on Martus legacy systems. However, this data is not public on the new Martus system To check if data from older bulletins is public on the old system search.",
 
+"button:ShowTrash=Trash",
 "button:export=Export",
 "button:help=Help",
 "button:create=Create",
