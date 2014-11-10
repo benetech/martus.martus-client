3f41080166dae528ac024ae6663bb754c57af740
nimaa <nimaa@localhost>
2014-11-10 12:08:33 -0500

    Pulled text out of fxml and moved into EnglishStrings

--- source/org/martus/client/swingui/EnglishStrings.java
+++ source/org/martus/client/swingui/EnglishStrings.java
@@ -505,6 +505,7 @@ public class EnglishStrings
 "button:ServerSyncRecordAuthor=Author",
 "button:ServerSyncRecordLastModified=Last Saved",
 "button:ServerSyncRecordSize=Size (Kb)",
+"button:HaveServerDefaulttoOn=Have Server Default to On? (will be applied on login)",
 
 "field:RecordLocationLocal=Local",
 "field:RecordLocationServer=Server",
