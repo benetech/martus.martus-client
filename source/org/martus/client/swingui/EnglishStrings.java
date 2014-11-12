0bb0a0a167cff6b85d4c9f9389a57b8d63ab49f7
nimaa <nimaa@localhost>
2014-11-12 14:50:04 -0500

    Manual EnglishStrings var name change along with its usage.

--- source/org/martus/client/swingui/EnglishStrings.java
+++ source/org/martus/client/swingui/EnglishStrings.java
@@ -507,7 +507,7 @@ public class EnglishStrings
 "button:ServerSyncRecordAuthor=Author",
 "button:ServerSyncRecordLastModified=Last Saved",
 "button:ServerSyncRecordSize=Size (Kb)",
-"button:HaveServerDefaulttoOn=Have Server Default to On? (will be applied on login)",
+"button:HaveServerDefaultToOn=Have Server Default to On? (will be applied on login)",
 "button:AutomaticallyDownloadFiles=Automatically Download Files from Server?",
 "button:ExportAccountPublicKey=Export Account Public Key",
 "button:Export3Key=Export 3 Key",
