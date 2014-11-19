be7efbcc20bd0cab49f6291ae740720e30779336
Kevin Smith <Kevin Smith@localhost>
2014-11-19 09:02:54 -0800

    Refactor extract local

---
+++ source/org/martus/client/swingui/jfx/generic/ImportBulletinAction.java
@@ -0,0 +1,157 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.generic;
+
+import java.io.File;
+
+import javafx.application.Platform;
+
+import javax.swing.JFileChooser;
+
+import org.martus.client.bulletinstore.BulletinFolder;
+import org.martus.client.core.MartusApp;
+import org.martus.client.swingui.MartusLocalization;
+import org.martus.client.swingui.UiMainWindow;
+import org.martus.client.swingui.actions.ActionDoer;
+import org.martus.client.swingui.filefilters.BulletinXmlFileFilter;
+import org.martus.client.swingui.filefilters.MartusBulletinArchiveFileFilter;
+import org.martus.client.swingui.jfx.landing.cases.FxCaseManagementController;
+import org.martus.client.tools.ImporterOfXmlFilesOfBulletins;
+import org.martus.clientside.FormatFilter;
+import org.martus.common.MartusLogger;
+
+public class ImportBulletinAction implements ActionDoer
+{
+	public ImportBulletinAction(FxCaseManagementController fxCaseManagementControllerToUse)
+	{
+		fxCaseManagementController = fxCaseManagementControllerToUse;
+		uiMainWindow = fxCaseManagementController.getMainWindow();
+	}
+
+	@Override
+	public void doAction()
+	{
+		JFileChooser fileChooser = new JFileChooser(getApp().getMartusDataRootDirectory());
+		fileChooser.setDialogTitle(getLocalization().getWindowTitle("ImportBulletin"));
+		FormatFilter mbaFileFilter = new MartusBulletinArchiveFileFilter(getLocalization());
+		FormatFilter xmlFileFilter = new BulletinXmlFileFilter(getLocalization());
+		fileChooser.addChoosableFileFilter(mbaFileFilter);
+		fileChooser.addChoosableFileFilter(xmlFileFilter);
+		fileChooser.setAcceptAllFileFilterUsed(true);
+
+		int userResult = fileChooser.showOpenDialog(getMainWindow());
+		if (userResult != JFileChooser.APPROVE_OPTION)
+			return;
+
+		importChooser(fileChooser);        
+	}
+	
+	private void importChooser(JFileChooser fileChooser)
+	{
+		File fileToImport = fileChooser.getSelectedFile(); 
+		if (fileToImport == null)
+			return;
+		
+		FormatFilter chosenExtensionFilter = (FormatFilter) fileChooser.getFileFilter();
+		if (FxInSwingContentController.isXmlExtensionSelected(getLocalization(), chosenExtensionFilter, fileToImport))
+			importBulletinFromXmlFile(fileToImport);
+		
+		if (FxInSwingContentController.isMbaExtensionSelected(getLocalization(), chosenExtensionFilter, fileToImport))
+			importBulletinFromMbaFile(fileToImport);
+	}
+	
+	private void importBulletinFromXmlFile(File fileToImport)
+	{
+		try
+		{
+			BulletinFolder folderToImportInto = getImportFolder();
+			ImporterOfXmlFilesOfBulletins importer = new ImporterOfXmlFilesOfBulletins(fileToImport, getApp().getStore(), folderToImportInto, System.out);
+			importer.importFiles();
+			updateCases();
+		}
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+		}
+	}
+
+	private void importBulletinFromMbaFile(File fileToImport)
+	{
+		try
+		{
+			BulletinFolder folderToImportInto = getImportFolder();
+			getApp().getStore().importZipFileBulletin(fileToImport, folderToImportInto, false);
+			updateCases();
+		}
+		catch (Exception e)
+		{
+			MartusLogger.logException(e);
+		}
+	}
+	
+	private void updateCases()
+	{
+		Platform.runLater(new UpdateCasesWorker());
+	}
+	
+	protected FxCaseManagementController getFxCaseManagementController()
+	{
+		return fxCaseManagementController;
+	}
+	
+	protected BulletinFolder getImportFolder()
+	{
+		return getApp().getStore().getFolderImport();
+	}
+	
+	private UiMainWindow getMainWindow()
+	{
+		return uiMainWindow;
+	}
+	
+	private MartusApp getApp()
+	{
+		return getMainWindow().getApp();
+	}
+	
+	private MartusLocalization getLocalization()
+	{
+		return getMainWindow().getLocalization();
+	}
+	
+	protected class UpdateCasesWorker implements Runnable
+	{
+		@Override
+		public void run()
+		{
+			BulletinFolder folderToImportInto = getImportFolder();
+			getFxCaseManagementController().updateCases(folderToImportInto.getName());
+		}
+	}
+	
+	private UiMainWindow uiMainWindow;
+	private FxCaseManagementController fxCaseManagementController;
+}
---
+++ source/org/martus/client/swingui/jfx/landing/bulletins/AttachmentTableRowData.java
@@ -0,0 +1,86 @@
+/*
+
+The Martus(tm) free, social justice documentation and
+monitoring software. Copyright (C) 2014, Beneficent
+Technology, Inc. (Benetech).
+
+Martus is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later
+version with the additions and exceptions described in the
+accompanying Martus license file entitled "license.txt".
+
+It is distributed WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, including warranties of fitness of purpose or
+merchantability.  See the accompanying Martus License and
+GPL license for more details on the required license terms
+for this software.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+*/
+package org.martus.client.swingui.jfx.landing.bulletins;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import org.martus.client.swingui.tablemodels.AttachmentTableModel;
+import org.martus.common.bulletin.AttachmentProxy;
+import org.martus.common.database.ReadableDatabase;
+
+public class AttachmentTableRowData
+{
+	public AttachmentTableRowData(AttachmentProxy attachmentToUse, ReadableDatabase database)
+	{
+		attachmentProxy = attachmentToUse;
+		name = new SimpleStringProperty(attachmentToUse.getLabel());
+		size =  new SimpleStringProperty(getSizeOfAttachment(attachmentToUse, database));
+		remove = new SimpleBooleanProperty(true);
+		view = new SimpleBooleanProperty(true);
+	}
+	
+	private String getSizeOfAttachment(AttachmentProxy attachmentToUse, ReadableDatabase database)
+	{
+		return AttachmentTableModel.getSize(attachmentToUse, database);
+	}
+
+	public SimpleStringProperty nameProperty()
+	{
+		return name;
+	}
+	
+	public SimpleStringProperty sizeProperty()
+	{
+		return size;
+	}
+	
+	public Property<Boolean> removeProperty() 
+    { 
+        return remove; 
+    }
+	
+	public Property<Boolean> viewProperty() 
+    { 
+        return view; 
+    }
+
+	public AttachmentProxy getAttachmentProxy()
+	{
+		return attachmentProxy;
+	}
+	
+	static public final String ATTACHMENT_NAME_PROPERTY_NAME = "name";
+	static public final String ATTACHMENT_SIZE_PROPERTY_NAME = "size";
+	static public final String ATTACHMENT_VIEW_PROPERTY_NAME = "view";
+	static public final String ATTACHMENT_REMOVE_PROPERTY_NAME = "remove";
+	private SimpleStringProperty name;
+	private SimpleStringProperty size;
+	private SimpleBooleanProperty view;
+	private SimpleBooleanProperty remove;
+	private AttachmentProxy attachmentProxy;
+ }
