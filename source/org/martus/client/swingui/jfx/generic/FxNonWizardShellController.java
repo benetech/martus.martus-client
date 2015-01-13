c28af31ba628fdd5e9d5c65985260461bba84e48
Kevin Smith <Kevin Smith@localhost>
2015-01-13 09:34:57 -0800

    Refactor rename class

--- source/org/martus/client/swingui/jfx/generic/FxNonWizardShellController.java
+++ source/org/martus/client/swingui/jfx/generic/FxNonWizardShellController.java
@@ -27,7 +27,7 @@ package org.martus.client.swingui.jfx.generic;
 
 import org.martus.client.swingui.UiMainWindow;
 
-public abstract class FxNonWizardShellController extends FxInSwingShellController
+public abstract class FxNonWizardShellController extends FxShellController
 {
 	public FxNonWizardShellController(UiMainWindow mainWindowToUse)
 	{
