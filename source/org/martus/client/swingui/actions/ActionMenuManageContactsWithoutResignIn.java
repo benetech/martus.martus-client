548480524074d516ec28a3e352014421a3840d8c
Kevin Smith <Kevin Smith@localhost>
2015-01-14 16:53:19 -0800

    Refactor rename class

--- source/org/martus/client/swingui/actions/ActionMenuManageContactsWithoutResignIn.java
+++ source/org/martus/client/swingui/actions/ActionMenuManageContactsWithoutResignIn.java
@@ -28,7 +28,7 @@ package org.martus.client.swingui.actions;
 import java.awt.event.ActionEvent;
 
 import org.martus.client.swingui.UiMainWindow;
-import org.martus.client.swingui.jfx.contacts.ContactsStage;
+import org.martus.client.swingui.jfx.contacts.FxInSwingContactsStage;
 
 public class ActionMenuManageContactsWithoutResignIn extends UiMenuAction  implements ActionDoer
 {
@@ -46,7 +46,7 @@ public class ActionMenuManageContactsWithoutResignIn extends UiMenuAction  imple
 	{
 		try
 		{
-			mainWindow.createAndShowLargeModalDialog(new ContactsStage(mainWindow));
+			mainWindow.createAndShowLargeModalDialog(new FxInSwingContactsStage(mainWindow));
 		} 
 		catch (Exception e)
 		{
