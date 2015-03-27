819e28ca0ce1a80a48e3d919f069ae57a1663d36
nimaa <nimaa@localhost>
2015-03-27 12:26:40 -0400

    No longer passing store to converter.  Converter is getting what it needs from Bulletin.

--- source/org/martus/client/core/FxBulletin.java
+++ source/org/martus/client/core/FxBulletin.java
@@ -85,7 +85,7 @@ public class FxBulletin
 		clear();
 		
 		if (b.isXFormsBulletin())
-			b = new BulletinFromXFormsLoader(b).createNewBulletinFromXFormsBulletin(store);
+			b = new BulletinFromXFormsLoader(b).createNewBulletinFromXFormsBulletin();
 		
 		universalIdProperty = new ReadOnlyObjectWrapper<UniversalId>(b.getUniversalId());
 		versionProperty = new SimpleIntegerProperty(b.getVersion());
