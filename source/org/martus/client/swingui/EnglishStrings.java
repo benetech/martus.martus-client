6fd39861e58e5318432c7f80eb0c465103241d94
nimaa <nimaa@localhost>
2015-03-27 12:33:40 -0400

    Passing Localization to converter.  Added translatable field name to secureApp section

--- source/org/martus/client/swingui/EnglishStrings.java
+++ source/org/martus/client/swingui/EnglishStrings.java
@@ -1930,6 +1930,7 @@ public class EnglishStrings
 "wintitle:CaseSent=Sent",
 "wintitle:CaseReceived=Received",
 "wintitle:CaseSaved=Saved",
+"field:secureAppDataSection=secureApp Section",
 
 "folder:%OutBox=Unsent Records",
 "folder:%Sent=Saved",
